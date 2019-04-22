41b4e16d3be477e1a40fb9d630c5a8a6956dc28a
sjscotton <sj.scotton@gmail.com>
2019-04-22 16:55:11 -0700

    Created table for displaying work info on work#index

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,9 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
+    books = Work.where(category: "book")
+    albums = Work.where(category: "album")
+    movies = Work.where(category: "movie")
+    @categories = [books, albums, movies]
   end
 end
