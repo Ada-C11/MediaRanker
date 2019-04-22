143988e05dced02ca70cd9684a654adc2bccfe10
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-22 16:17:24 -0700

    Added models, controllers and routes

---
+++ db/migrate/20190422231018_create_users_works_join.rb
@@ -0,0 +1,8 @@
+class CreateUsersWorksJoin < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users_works_joins do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :work, index: true
+    end
+  end
+end
