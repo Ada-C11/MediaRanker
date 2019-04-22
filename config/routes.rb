d1724daed0944381c0158d7df74be587b3fd45c7
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-22 15:41:53 -0700

    added works controller and set-up for CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'works/index'
+  get 'works/show'
+  get 'works/new'
+  get 'works/create'
+  get 'works/edit'
+  get 'works/update'
+  get 'works/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
