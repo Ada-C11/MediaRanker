3ebadb16a25adca90cfd82e28ca9c5b98b53661a
qqdipps <sdipp007@gmail.com>
2019-04-22 16:11:58 -0700

    updated models with relationship has_and_belongs_to_many

--- db/migrate/20190422225624_create_join_table_user_work.rb
+++ db/migrate/20190422225624_create_join_table_user_work.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableUserWork < ActiveRecord::Migration[5.2]
   def change
     create_join_table :users, :works do |t|
-      # t.index [:user_id, :work_id]
+      t.index [:user_id, :work_id]
       # t.index [:work_id, :user_id]
     end
   end
