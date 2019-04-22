938a1f87c00e88660d59191d3e7232f1cc24c45c
Angela <angela.ohh@gmail.com>
2019-04-22 16:54:27 -0700

    added create show and new actiosn for works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,31 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    @works = Work.all.order(:title)
+  end
+
+  def show
+    @work = Work.find_by(params[:id])
+    if @work.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.new(work_params)
+    if work.save
+      redirect_to work_path(work.id)
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
