396c84e9cbac6a2c004b7db748928a6a948e48c2
Tatiana Quintana <tatsqui@gmail.com>
2019-04-22 16:06:33 -0700

    started 7 resftul controller actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,34 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show 
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def work_params
+    params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root "homepages#index"
 
   resources :works
+  
+  # will need a nested route for upvotes
 
 end
