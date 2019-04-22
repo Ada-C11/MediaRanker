63e6658343a1db5ad172c5ad1a03a88933104ad1
Grace Shea <grace.m.shea@gmail.com>
2019-04-22 16:50:18 -0700

    database reset

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,14 +3,40 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
-  end
-
   describe "Relationships" do
   end
 
   describe "Validations" do
+    it "is valid when all fields are present" do
+      expect(work).must_be :valid?
+    end
+
+    it "must have a title" do
+      work.title = nil
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :title
+    end
+
+    it "must have a unique title for the same category" do
+      other_work = Work.new title: work.title, category: "book"
+      other_work.save
+
+      valid = other_work.valid?
+
+      expect(valid).must_equal false
+      expect(other_work.errors.messages).must_include :title
+    end
+
+    it "can have the same title for different categories" do
+      other_work = Work.new title: work.title, category: "album"
+      other_work.save
+
+      valid = other_work.valid?
+
+      expect(valid).must_equal true
+    end
   end
 
   describe "Custom Methods" do
