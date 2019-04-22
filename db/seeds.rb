c5bf688a7a5869b4cebdda895b33a123e2950d0e
Tatiana Quintana <tatsqui@gmail.com>
2019-04-22 15:25:30 -0700

    seeded database with csv seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,14 @@ require 'csv'
 csv_text = File.read(Rails.root.join('db', 'media_seeds.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  puts row.to_hash
+  work = Work.new
+  work.category = row['category']
+  work.title = row['title']
+  work.creator = row['creator']
+  work.publication_year = row['publication_year']
+  work.description = row['description']
+  work.save
+  puts "#{work.category}, #{work.title} saved."
 end
+
+puts "There are now #{Work.count} rows in the Media (works) table"
