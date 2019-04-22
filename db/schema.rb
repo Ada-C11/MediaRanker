95fa21a31fa5d944021f7c20c2adec8c484760f8
qqdipps <sdipp007@gmail.com>
2019-04-22 16:02:53 -0700

    added works model and join table works_users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_224858) do
+ActiveRecord::Schema.define(version: 2019_04_22_225624) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users_works", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "work_id", null: false
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "category"
+    t.string "title"
+    t.string "creator"
+    t.integer "publication_year"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
