804758309791e0602446920b1a66549b6981dc66
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-22 17:25:35 -0700

    Added method skeleton to users and works controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,69 @@ class UsersController < ApplicationController
     @users = Users.all.sort_by(&:id)
   end
   
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    user_id = params[:id]
+    @user = User.find(user_id)
+    @works = Work.where(user_id: user_id.to_i)
+  end
+
+  def edit
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    redirect_to user_path if @user.nil?
+  end
+
+  def update
+    begin
+      user_id = params[:id]
+      user = User.find(user_id)
+    rescue
+      flash[:error] = "Could not find user with id: #{params['id']}"
+      redirect_to user_path(user_id)
+      return
+    end
+
+    if user.update(user_params)
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    begin
+      user = User.find(params[:id])
+    rescue
+      flash[:error] = "Could not find user with id: #{params['id']}"
+      redirect_to users_path
+      return
+    end
+    user.destroy
+    redirect_to users_path
+  end
+
 end
+
+private
+
+def user_params
+  params.require(:user).permit(
+    :username,
+    :votes
+  )
+end
+  
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,4 +3,75 @@ class WorksController < ApplicationController
   def index
     @works = Works.all.sort_by(&:id)
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    work_id = params[:id]
+    @work = Work.find(work_id)
+    @users = User.where(work_id: work_id.to_i)
+  end
+
+  def edit
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    redirect_to work_path if @work.nil?
+  end
+
+  def update
+    begin
+      work_id = params[:id]
+      work = Work.find(work_id)
+    rescue
+      flash[:error] = "Could not find work with id: #{params['id']}"
+      redirect_to work_path(work_id)
+      return
+    end
+
+    if work.update(work_params)
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    begin
+      work = Work.find(params[:id])
+    rescue
+      flash[:error] = "Could not find work with id: #{params['id']}"
+      redirect_to works_path
+      return
+    end
+    work.destroy
+    redirect_to works_path
+  end
+
+end
+
+private
+
+def work_params
+  params.require(:work).permit(
+    :category,
+    :title,
+    :creator,
+    :publication_year,
+    :description,
+    :votes
+  )
+  end
+
 end
