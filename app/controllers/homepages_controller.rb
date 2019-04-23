0194dc2cd541d452ae7a52abd5bfcd614eecc0bc
Bitaman <b.amani.s@gmail.com>
2019-04-23 16:26:08 -0700

    added the spotlight and the top 10 to the homepage

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,2 +1,24 @@
 class HomepagesController < ApplicationController
+  def index
+    @spotlight = Work.all.sample
+    puts "inside homepage"
+    puts top_ten_movies.inspect
+    top_ten_books
+    top_ten_albums
+  end
+
+  def top_ten_movies
+    @movies = Work.all.where(category: "movie")
+    @top_ten_movies = @movies.sample(10)
+  end
+
+  def top_ten_albums
+    @albums = Work.all.where(category: "album")
+    @top_ten_albums = @albums.sample(10)
+  end
+
+  def top_ten_books
+    @books = Work.all.where(category: "book")
+    @top_ten_books = @books.sample(10)
+  end
 end
---
+++ app/models/homepage.rb
@@ -0,0 +1,5 @@
+class Homepage < ApplicationRecord
+
+
+
+end
