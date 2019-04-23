c620dd8a9e9064c86e5976aeb436f30167ee8771
Kate Sandler <kate@katekatekate.net>
2019-04-23 15:22:41 -0700

    Adds user model tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index" do
+    get users_index_url
+    value(response).must_be :success?
+  end
+
+  it "should get show" do
+    get users_show_url
+    value(response).must_be :success?
+  end
 end
