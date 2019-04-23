a44c14e96cf5757c3f6033d9a33dc80ca2d9f801
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-23 15:59:00 -0700

    updated routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   # update to homepage once done
   root to: "works#index"
 
   resources :works
+  resources :users, only: [:index, :show]
 end
