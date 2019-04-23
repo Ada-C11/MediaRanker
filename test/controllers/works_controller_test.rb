702e4d6cc1f03f9ab7d347e92c625ca699efe248
Carla Bosco <carlabosco@gmail.com>
2019-04-22 20:28:28 -0700

    Added initial test for WorksController

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it 'can list all media' do
+    get works_path
+    must_respond_with :success
+  end
 end
