4442019d9274167f8bd2b1623ffbf5ba502d5b85
Angela <angela.ohh@gmail.com>
2019-04-23 14:51:31 -0700

    fixed partial form for new action

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,17 @@
 class Work < ApplicationRecord
+  def spotlight
+    return self.sample
+  end
+
+  def top_book
+    return self.sample
+  end
+
+  def top_album
+    return self.sample
+  end
+
+  def top_movie
+    return self.sample
+  end
 end
