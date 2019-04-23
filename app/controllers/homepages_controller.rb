2673424adaacbba1ec575f018373cbb22cad62e3
Erica Norris <norrise120@gmail.com>
2019-04-23 16:43:06 -0700

    Updated homepage controller and html to include work data and featured work

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,4 +1,8 @@
 class HomepagesController < ApplicationController
   def index
+    @featured = Work.all.sample
+    @books = Work.where(category: "book")
+    @albums = Work.where(category: "album")
+    @movies = Work.where(category: "movie")
   end
 end
