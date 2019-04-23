d051ac48561c577956ad6087bc96ac510cfc38cd
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-22 20:38:23 -0700

    edited yml file and added show method

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,14 @@
 class WorksController < ApplicationController
-    def index
-        @works = Work.all 
-    end
+  def index
+    @works = Work.all
+  end
 
-    
+  def show
+    work_id = params[:id].to_i
+    @work = Work.find_by(id: work_id)
+
+    if @work.nil?
+      redirect_to works_path
+    end
+  end
 end
