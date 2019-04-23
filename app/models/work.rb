225af48aab5352fb5cbf7d219a2439db98385247
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-23 16:05:15 -0700

    added homepage

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,4 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: "homepages#index"
+  get "homepages/index"
   resources :works
 end
