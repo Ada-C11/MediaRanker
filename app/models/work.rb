26851d2374c8d3494bcd946045d5e7dca64870d0
Kate Sandler <kate@katekatekate.net>
2019-04-22 23:08:30 -0700

    Adds spotlight work functionality

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,29 @@
 class Work < ApplicationRecord
+  has_many :votes
+
+  validates :title, presence: true
+
+  def self.movies_list
+    return Work.select { |work| work.category == 'movie' }
+  end
+
+  def self.books_list
+    return Work.select { |work| work.category == 'book' }
+  end
+
+  def self.albums_list
+    return Work.select { |work| work.category == 'album' }
+  end
+
+  def self.spotlight
+    media_spotlight = Work.first
+    max_votes = Work.first.votes.length
+    Work.all.each do |work|
+      if work.votes.length > max_votes
+        max_votes = work.votes.length
+        media_spotlight = work
+      end
+    end
+    return media_spotlight
+  end
 end
