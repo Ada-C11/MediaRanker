e4448b53df606747c4011eb604bc64abb89dc2d5
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-23 14:51:56 -0700

    Added some styles to nav bar and lioked to view all works

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/works', to: 'works#index', as: 'works'
