9bc06d7b56ff55c3684801d1e22ab46c672261c1
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-23 15:26:42 -0700

    formatted edit and new forms

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -7,4 +7,20 @@ class HomepagesController < ApplicationController
     top_ten_books
     top_ten_albums
   end
+  
+  def top_ten_movies
+    @movies = Work.where(category: "movie")
+    @top_ten_movies = @movies.sample(10)
+  end
+
+  def top_ten_books
+    @books = Work.where(category: "book")
+    @top_ten_books = @books.sample(10)
+  end
+
+  def top_ten_albums  
+    @albums = Work.where(category: "album")
+    @top_ten_albums = @albums.sample(10)
+  end
+
 end
--- app/models/homepage.rb
+++ app/models/homepage.rb
@@ -1,17 +1,4 @@
 class Homepage < ApplicationRecord
-  def top_ten_movies
-    @movies = Work.where(category: "movie")
-    @top_ten_movies = @movies.sample(10)
-  end
 
-  def top_ten_books
-    @books = Work.where(category: "book")
-    @top_ten_books = @books.sample(10)
-  end
-
-  def top_ten_albums  
-    @albums = Work.where(category: "album")
-    @top_ten_albums = @albums.sample(10)
-  end
 
 end
