27e9eeebfe10cdb83f9203cd43196ab6fdeb5924
Bitaman <b.amani.s@gmail.com>
2019-04-23 14:55:11 -0700

    added validation and teasts for work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  # let(:work) { Work.new }
+  describe "validations" do
+    before do
+      @work = Work.new(
+        title: "some unique test title",
+      )
+    end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it "passes validations with good data" do
+      expect(@work).must_be :valid?
+    end
+
+    it "rejects work with the same title" do
+      # arrange
+      duplicate_title = Work.first.title
+      @work.title = duplicate_title
+
+      # act
+      result = @work.valid?
+
+      # assert
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
   end
 end
