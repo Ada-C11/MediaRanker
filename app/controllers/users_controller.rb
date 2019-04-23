3816ceed44603045ede7735fcf28d1a119766e2d
Amy Phung <phung.amy85@gmail.com>
2019-04-23 14:35:26 -0700

    Add login/logout User methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,47 @@ class UsersController < ApplicationController
   def create
   end
   
+  def login_form
+    @user = User.new
+  end
+  
+  def login
+    username = params[:user][:name]
+    @user = User.find_by(username: username)
+    
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back, #{@user.name}!"
+      redirect_back(fallback_location: root_path)
+    else
+      @user = User.create!(name: username)
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the Mediaranker community, #{@user.name}!"
+    end
+    
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def current
+    @current_user = User.find_by(id: session[user_id])
+      
+    unless @current_user
+      redirect_to login_path
+      flash[:error] = "Could not find user. Try logging in again."
+    end
+  end
+  
+  def logout
+    @user = User.find_by(id: params[:id])
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out."
+    
+    redirect_to root_path
+  end
+  
   private
   
   def user_params
-    return params.require(:user).permit(:name,)
+    return params.require(:user).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,11 @@ Rails.application.routes.draw do
     resources :votes
   end
 
-  resources :users do
-    resources :votes
-    # , only: [:index, :new]
-  end
+  resources :users 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 
   resources :votes
 
