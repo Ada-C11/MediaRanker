e3f44c8be02216f30e88081fdc028219f6e8b4ae
Grace Shea <grace.m.shea@gmail.com>
2019-04-23 16:17:11 -0700

    CRUD operations

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,23 +11,57 @@ class WorksController < ApplicationController
     @work = Work.new work_params
 
     if @work.save
-      redirect_to work_path(@work.id)
+      redirect_to work_path(@work.id), { :flash => { :success => "Successfully created work" } }
     else
-      render :new, status: :bad_request
+      render :new, status: :bad_request, :flash.now => { :error => "Failed to create work" }
     end
   end
 
   def show
-    @work = Work.find(params[:id])
+    @work = Work.find_by(id: params[:id])
 
     unless @work
-      head :not_found
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work
+      if @work.update work_params
+        redirect_to work_path(@work.id), { :flash => { :success => "Successfully updated work" } }
+      else
+        redirect_to :edit, :flash => { :error => "Failed to update work" }
+      end
+    else
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def destroy
+    @work = work.find_by(id: params[:id])
+    if @work
+      if @work.destroy
+        redirect_to root_path, { :flash => { :success => "Successfully deleted work" } }
+      else
+        redirect_to root_path, :flash => { :error => "Failed to delete work" }
+      end
+    else
+      redirect_to root_path, status: 302, :flash => { :error => "Could not find work with id: #{params[:id]}" }
     end
   end
 
   private
 
   def work_params
-    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+    return params.require(:work).permit(:category, :title, :creator, :pub_yr, :description)
   end
 end
