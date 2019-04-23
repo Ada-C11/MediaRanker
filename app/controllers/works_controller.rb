1726e129b41ef4109438959f42cc1a873b4da994
Bitaman <b.amani.s@gmail.com>
2019-04-22 21:17:34 -0700

    added the index and show actions to workscontroller and created the index and show erb files

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,26 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+    @featured_work = @works.sample
+  end
+
+  def show
+    work_id = params[:id]
+
+    @work = Work.find_by(id: work_id)
+
+    unless @work
+      head :not_found
+    end
+  end
+
+  def edit
+    work_id = params[:id]
+
+    @work = Work.find_by(id: work_id)
+
+    unless @work
+      head :not_found
+    end
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,51 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    @work = Work.create(category: "book", title: "test book", creator: "test creator", publication_date: 1996, description: "a good test item")
+  end
+  describe "index" do
+    it "renders without crashing" do
+      get works_path
+      must_respond_with :ok
+    end
+    it "renders even if there are zero books" do
+      # Arrange
+      Work.destroy_all
+
+      # Act
+      get works_path
+
+      # Assert
+      must_respond_with :ok
+    end
+  end
+  describe "show" do
+    it "returns a 404 status code if the works doesn't exist" do
+      # TODO come back to this
+      work_id = 12345
+
+      get work_path(work_id)
+
+      must_respond_with :not_found
+    end
+    it "works for a work that exists" do
+      get work_path(@work.id)
+
+      # Assert
+      must_respond_with :ok
+    end
+  end
+  describe "edit" do
+    it "responds with OK for a real real" do
+      get edit_work_path(@work)
+      must_respond_with :ok
+    end
+
+    it "responds with NOT FOUND for a fake work" do
+      work_id = Work.last.id + 1
+      get edit_work_path(work_id)
+      must_respond_with :not_found
+    end
+  end
 end
