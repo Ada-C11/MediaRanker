62cd90dc7083bbebe56d68ab68d12dfc69a55245
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-23 12:23:28 -0700

    table index works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,12 @@
 class Work < ApplicationRecord
+  def self.category(works)
+    hash = Hash.new { }
+    works.each do |work|
+      category = work.category
+      if !hash.include?(category)
+        hash[category] = 1
+      end
+    end
+    return hash.keys.sort
+  end
 end
