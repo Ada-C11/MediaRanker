8787faa4e6b54c3bfe5444d2b5c44cd4d8698cde
Kate Sandler <kate@katekatekate.net>
2019-04-23 15:59:23 -0700

    Routes for all sessions methods and upvote added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
+
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+  get 'sessions/login', to: 'sessions#new'
+  post 'sessions/login', to: 'sessions#login', as: 'login'
+  delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
+
+  get 'users/index', to: 'users#index', as: 'users'
+  get 'users/show', to: 'users#show', as: 'user'
+
   root 'landing#index'
 
   resources :works
+  get 'works/:id/upvote', to: 'works#upvote', as: 'upvote_work'
   resources :users, only: [:index, :show, :new]
 end
