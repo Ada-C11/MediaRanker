3300f86e8f876a17ae5e74f765ae1ca5636cab48
sjscotton <sj.scotton@gmail.com>
2019-04-22 17:53:44 -0700

    Added show work action, view, and tests

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -6,4 +6,12 @@ class WorksController < ApplicationController
     movies = Work.where(category: "movie")
     @categories = [books, albums, movies]
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      redirect_to works_path
+    end
+  end
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -5,7 +5,28 @@ describe WorksController do
     it "can get to the root path" do
       get root_path
 
-      must_respont_with :success
+      must_respond_with :success
+    end
+
+    it "can get to the index path" do
+      get works_path
+
+      must_respond_with :success
+    end
+  end
+  describe "show" do
+    it "can get a valid work" do
+      work = works(:one)
+
+      get work_path(work.id)
+
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid work" do
+      get work_path(-1)
+
+      must_respond_with :redirect
     end
   end
 end
