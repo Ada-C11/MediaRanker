f902c9399694f346d9d6ada213d199d739e8a716
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-23 08:05:29 -0700

    added show and testsand basic html to show media details

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,38 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+  
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      redirect_to works_path
+      return
+    end
+
+    # @unrated_trip = @passenger.trips.find_by(rating: nil)
+    # @trip = Trip.new
+    # @trips = @passenger.trips
+  end
+
+#   def new
+#     @work = Work.new
+#   end
+# 
+#   def create
+#     @passenger = Passenger.new passenger_params
+#     successful = @passenger.save
+#     if successful
+#       redirect_to passengers_path
+#     else
+#       render :new
+#     end
+#   end
+
+#   def edit
+#     @passenger = Passenger.find_by(id: params[:id])
+#     unless @passenger
+#       redirect_to passengers_path
+#     end
+#   end    
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -8,4 +8,21 @@ describe WorksController do
       must_respond_with :success
     end
   end
+
+  describe "show" do
+    it "can get the details page" do
+      work = works(:custom1)
+      get work_path(work.id)
+
+      must_respond_with :success
+    end
+
+    it "will redirect if given an invalid work ID" do
+      get work_path(-1)
+
+      must_respond_with :redirect
+    end
+  end
 end
+
+#validate things? use fixtures or let?
