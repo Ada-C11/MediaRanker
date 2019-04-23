8c2cd6242dc0b9b365972e83da17739993fe8334
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-23 16:43:43 -0700

    updated erb forms and edit and new and create and update methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,20 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    successful = @work.save
+    if successful
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
   def show
     @work = Work.find_by(id: params[:id])
 
@@ -18,24 +32,27 @@ class WorksController < ApplicationController
     # @trips = @passenger.trips
   end
 
-  #   def new
-  #     @work = Work.new
-  #   end
-  #
-  #   def create
-  #     @passenger = Passenger.new passenger_params
-  #     successful = @passenger.save
-  #     if successful
-  #       redirect_to passengers_path
-  #     else
-  #       render :new
-  #     end
-  #   end
-
   def edit
     @work = Work.find_by(id: params[:id])
     unless @work
       redirect_to works_path, flash: { error: "Could not find media with id: #{params[:id]}" }
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    unless @work
+      head :not_found
+      return
+    end
+    if @work.update(work_params)
+      flash[:status] = :success
+      flash[:message] = "Successfully updated media #{@work.id}"
+      redirect_to work_path(@work)
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not save work #{@work.id}"
+      render :edit, status: :bad_request
     end
   end
 
@@ -50,5 +67,11 @@ class WorksController < ApplicationController
     flash[:status] = :success
     flash[:message] = "Successfully deleted work #{work.id}"
     redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :works, only: [:index, :show, :edit, :destroy]
+  resources :works, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
