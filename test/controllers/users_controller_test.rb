a1c970c68b0cc5b927243644fcc8b49eacdeded2
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-23 16:44:49 -0700

    added testing for show action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 describe UsersController do
+  let(:kim) { users(:kim) }
+  let(:aj) { useres(:aj) }
+
   describe "index" do
     it "can get index" do
       get users_path
@@ -10,10 +13,22 @@ describe UsersController do
   end
 
   describe "show" do
-    it "can get the show for a valid/logged in user" do
+    it "can show an existing valid user" do
+      valid_id = kim.id
+
+      get user_path(valid_id)
+
+      must_respond_with :success
     end
 
-    it "redirects to the root path for invalid/logged out user" do
+    it "redirects to the root path for an invalid user id" do
+      invalid_id = -1
+
+      get user_path(invalid_id)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+      expect(flash[:failure]).must_equal "User Not Found."
     end
   end
 end
