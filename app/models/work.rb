2423fa395f0b629d002dd29de907dcbc782cce43
Kate Sandler <kate@katekatekate.net>
2019-04-23 14:54:15 -0700

    Refactors while I'm waiting for something to load

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,18 +3,6 @@ class Work < ApplicationRecord
 
   validates :title, presence: true
 
-  def self.movies_list
-    return Work.select { |work| work.category == 'movie' }
-  end
-
-  def self.books_list
-    return Work.select { |work| work.category == 'book' }
-  end
-
-  def self.albums_list
-    return Work.select { |work| work.category == 'album' }
-  end
-
   def self.top_media
     media_spotlight = Work.first
     max_votes = Work.first.votes.length
@@ -27,37 +15,15 @@ class Work < ApplicationRecord
     return media_spotlight
   end
 
-  def self.top_movies
-    top_10 = []
-    movies = Work.movies_list.sort_by { |work| work.vote_count }
-    movies.reverse!
-
-    10.times do |i|
-      top_10 << movies[i]
-    end
-    return top_10
+  def self.category_list(category_type)
+    category_list =  Work.select { |work| work.category == category_type }
+    return category_list.sort_by { |work| work.vote_count }.reverse!
   end
 
-  def self.top_books
-    top_10 = []
-    books = Work.books_list.sort_by { |work| work.vote_count }
-    books.reverse!
+  def self.top_ten(category_type)
+    works = Work.category_list(category_type)
 
-    10.times do |i|
-      top_10 << books[i]
-    end
-    return top_10
-  end
-
-  def self.top_albums
-    top_10 = []
-    albums = Work.albums_list.sort_by { |work| work.vote_count }
-    albums.reverse!
-
-    10.times do |i|
-      top_10 << albums[i]
-    end
-    return top_10
+    return works.first(10)
   end
 
   def vote_count
