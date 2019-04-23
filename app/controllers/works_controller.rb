0f69ab997a01cff4be943a87a44dc375c14b6a1e
Tatiana Quintana <tatsqui@gmail.com>
2019-04-22 19:52:44 -0700

    added basic controller actions to works, and corrected typo in works#index view

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,27 +4,57 @@ class WorksController < ApplicationController
   end
 
   def new
-
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
 
+    successful = @work.save
+
+    if successful
+      redirect_to works_path
+    else
+      render :new, status :bad_request
+    end
   end
 
   def show 
+    @work = Work.find_by(id: params[:id])
 
+    unless @work
+      head :not_found
+    end
   end
 
   def edit
+    @work = Work.find_by(id: params[:id])
 
+    unless @work
+      head :not_found
+    end
   end
 
   def update
+    @work = Work.find_by(id: params[:id])
 
+    unless @work
+      head :not_found
+    end
+
+    if @work.update(work_params)
+      redirect_to work_path(@work)
+    else
+      render :edit, status :bad_request
+    end
   end
 
   def destroy
+    work = Work.find_by(id: params[:id])
 
+    work.destroy
+
+    redirect_to root_path
   end
 
   def work_params
