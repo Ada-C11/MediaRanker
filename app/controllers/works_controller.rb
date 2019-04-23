49b6f8aa7f906d0cabe362502a97fa4e61b22f62
Shubha Rajan <shubharajan94@gmail.com>
2019-04-23 14:53:57 -0700

    added update/delete methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,9 +11,9 @@ class WorksController < ApplicationController
     @work = Work.new work_params
 
     if @work.save
-      redirect_to work_path(@work.id)
+      redirect_to work_path(@work.id), { :flash => { :success => "Successfully created work!" } }
     else
-      render :new, status: :bad_request
+      render :new, status: :bad_request, :flash.now => { :error => "Failed to create work" }
     end
   end
 
@@ -21,7 +21,40 @@ class WorksController < ApplicationController
     @work = Work.find(params[:id])
 
     unless @work
-      head :not_found
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def edit
+    @work = Work.find(params[:id])
+
+    unless @work
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def update
+    if @work
+      if @work.update work_params
+        redirect_to work_path(@work.id), { :flash => { :success => "Successfully updated work!" } }
+      else
+        redirect_to :edit, :flash => { :error => "Failed to update work" }
+      end
+    else
+      redirect_to root_path, :flash => { :error => "Could not find work with id: #{params[:id]}" }
+    end
+  end
+
+  def delete
+    @work = work.find_by(id: params[:id])
+    if @work
+      if @work.destroy
+        redirect_to root_path, { :flash => { :success => "Successfully deleted work!" } }
+      else
+        redirect_to root_path, :flash => { :error => "Failed to delete work" }
+      end
+    else
+      redirect_to root_path, status: 302, :flash => { :error => "Could not find work with id: #{params[:id]}" }
     end
   end
 
