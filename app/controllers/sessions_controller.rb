1121277c76e422ae41a8ead38bd351fbab4e829e
Kate Sandler <kate@katekatekate.net>
2019-04-23 15:58:22 -0700

    Add the destroy, login, and new views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
   def login
+    user = User.find_by(name: params[:user][:name])
+
+    if user.nil?
+      user = User.create(name: params[:author][:name])
+    end
+
+    session[:user_id] = user.id
+    flash[:sucess] = "Successfully logged in as #{user.name}"
+    redirect_to root_path
   end
 
-   def new
+  def new
+    @user = User.new
   end
 
-   def destroy
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_back fallback_location: root_path
   end
 end
