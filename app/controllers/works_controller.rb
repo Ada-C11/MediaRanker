909629be5203a13527a410c572da56f22eb8594a
Jessica <jessica.homet@gmail.com>
2019-04-23 15:53:21 -0700

    Add styling, new, create, and show for work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,12 +3,32 @@ class WorksController < ApplicationController
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
+
+    if !@work
+      head :not_found
+    end
   end
 
   def new
+    @work = Work.new
   end
 
   def create
+    @work = Work.new(work_params)
+
+    is_successful = @work.save
+
+    if is_successful
+      flash[:success] = "Successfully created #{@work.category} #{@work.id}"
+      redirect_to work_path(@work.id)
+    else
+      @work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+
+      render :new, status: :bad_request
+    end
   end
 
   def edit
@@ -18,5 +38,11 @@ class WorksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
   end
 end
