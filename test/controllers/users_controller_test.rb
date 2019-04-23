6a59c9f97e676cf4c5317533917f2550b5c939f9
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-23 16:28:22 -0700

    added testing for index action

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,19 @@
 require "test_helper"
 
 describe UsersController do
-  it "should get index" do
-    get users_index_url
-    value(response).must_be :success?
+  describe "index" do
+    it "can get index" do
+      get users_path
+
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get users_show_url
-    value(response).must_be :success?
-  end
+  describe "show" do
+    it "can get the show for a valid/logged in user" do
+    end
 
+    it "redirects to the root path for invalid/logged out user" do
+    end
+  end
 end
