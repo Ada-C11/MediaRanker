75da106f50ffc4943139d187ced9b63a23e52d03
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-23 14:25:24 -0700

    Added homepages model and moved top ten methods there

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_231018) do
+ActiveRecord::Schema.define(version: 2019_04_23_212243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
