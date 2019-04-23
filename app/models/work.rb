09a4f07799ccce549cca73cf76e247822a28d87b
Grace Shea <grace.m.shea@gmail.com>
2019-04-23 09:46:52 -0700

    sample 10 works in each category added to work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  def self.top_ten(category)
+    if self.where(category: category).length > 10
+      return self.where(category: category).sample(10)
+    else
+      return self.where(category: category)
+    end
+  end
 end
