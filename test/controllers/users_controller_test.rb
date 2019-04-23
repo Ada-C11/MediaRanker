92bc4ba4b3b2322bbf96fe6b2dd803b42fd5daa8
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-23 15:31:24 -0700

    started testing for login and logout methods in user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  it "gets the login path" do
+    get login_path
+
+    must_respond_with :success
+  end
+
+  # it "successfully logs a user in" do
+  #   username = "cyndi"
+  #   user = User.create(username: username)
+
+  #   expect {
+  #     post login_path(user)
+  #   }.must_change "User.count", +1
   # end
 end
