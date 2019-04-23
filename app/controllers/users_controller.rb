cac26d6fd2f7dbd5c39f302ff08720b8f1adee9d
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-23 16:44:24 -0700

    added show action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+
+    if !@user
+      flash[:failure] = "User Not Found."
+      redirect_to root_path
+    end
   end
 end
