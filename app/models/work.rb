c1382ae3745eb27ad04d4518154910c6afa1d20b
MyriamWD <myriam.waldend@gmail.com>
2019-04-23 23:58:55 -0700

    Built and index page with a list of all works for each category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,11 @@
 class Work < ApplicationRecord
+  def self.albums
+    where(category: "album")
+  end
+  def self.books
+    where(category: "book")
+  end
+  def self.movies
+    where(category: "movie")
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+MEDIA_SEEDS_FILE = Rails.root.join("db", "media_seeds.csv")
+puts "Loading raw media data from #{MEDIA_SEEDS_FILE}"
+
+work_failures = []
+CSV.foreach(MEDIA_SEEDS_FILE, :headers => true) do |row|
+  work = Work.new
+  # no id in csv file
+  work.category = row["category"]
+  work.title = row["title"]
+  work.creator = row["creator"]
+  work.publication_year = row["publication_year"]
+  work.description = row["description"]
+  successful = work.save
+  if !successful
+    work_failures << work
+    puts "Failed to save work: #{work.inspect}"
+  else
+    puts "Created work: #{work.inspect}"
+  end
+end
+
+puts "Added #{Work.count} work records"
+puts "#{work_failures.length} works failed to save"
