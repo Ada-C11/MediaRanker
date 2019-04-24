b98c386f1ba04c7b1a79a6ce5bd7e8f8852327b3
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-23 20:15:39 -0700

    added test for homepage index

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe HomepagesController do
   it "should get index" do
     get homepages_index_url
-    value(response).must_be :success?
-  end
 
+    must_respond_with :success
+  end
 end
