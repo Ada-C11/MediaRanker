780fa80606c66a9485f85f756043fe0f81170999
kateannnichols <kateannnichols@gmail.com>
2019-04-23 22:03:22 -0700

    set up controllers and restful routes for works controller

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class WorksController < ApplicationController
+  before_action :find_work, except: %i[index new create]
+
+  def index
+    @work = Work.ordered
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params[:work])
+
+    if @work.save
+      render :show
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    @work.update(work_params[:work])
+    if @work.save
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+
+  def upvote
+    Work.upvote(@work)
+    redirect_to work_path(@work.id)
+  end
+
+  def destroy
+    @move.destroy
+    redirect_to work_path
+  end
+
+    private
+
+  def find_work
+    @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params.permit(work: %i[title creator publication_year])
+  end
+  end
