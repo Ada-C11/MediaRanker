6707bcb416ce88553ff4a6027528b888ea7f5a84
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-23 21:53:50 -0700

    added current user and logout functionality

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,11 +1,22 @@
 require "test_helper"
 
 describe UsersController do
+  let (:user_one) {
+    users(:user_one)
+  }
   describe "login_form" do
     it "should get login form" do
       get login_path
 
       must_respond_with :success
-    end 
-  end 
+    end
+  end
+
+  describe "login" do
+    it "can log in an existing user" do
+    end
+
+    it "can log in a new user" do
+    end
+  end
 end
