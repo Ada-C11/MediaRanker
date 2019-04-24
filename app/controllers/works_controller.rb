cf26fd997c78a3b3b9705deb09cfe0341cb224d2
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-23 21:22:57 -0700

    added Works#show

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,11 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+    if !@work
+      head :not_found
+    end
+  end
 end
