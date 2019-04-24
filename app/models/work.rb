192c336f4872c298ebfa3035bacff412dafd8467
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-24 08:57:31 -0700

    Added controller tests for Work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,7 @@
 class Work < ApplicationRecord
-  
+  validates :category, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :description, presence: true, uniqueness: true
+  validates :publication_year, presence: true
 end
