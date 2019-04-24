d46ba894dbd80cd4c5f570f55ec8d461d9b74ee3
kateannnichols <kateannnichols@gmail.com>
2019-04-24 09:33:19 -0700

    update users controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
-end
+    before_action :find_work, except: %i[index new create]
+
+    def login_form
+        @user = User.new
+        end
+    
+      def login
+        username = params[:user][:username]
+        user = User.find_by(username: username)
+        flash_msg = if user.nil?
+                      'Welcome new user!'
+                    else
+                      "Welcome back #{username}!"
+                    end
+        user ||= User.create(username: username)
+        session[:user_id] = user.user_id
+        flash[:success] = "#{username} is logged in."
+        redirect_to root_path
+      end
+    
+      def current
+        @user = User.find_by(id: session[:user_id])
+      end
+    end
