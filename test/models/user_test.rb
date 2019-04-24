2d542fee112dba78a37e4e55b5382b61d82d3ab2
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-24 15:39:52 -0700

    user controller tests for session id and request to users current

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 describe User do
   let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  describe "validations" do
+    it "rejects validations with bad data" do
+      expect(user.valid?).must_equal false
+      expect(user.errors.messages[:username][0]).must_equal "can't be blank"
+    end
+
+    it "passes validations with good data" do
+      user = users(:custom1)
+      expect(user).must_be :valid?
+    end
   end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,17 @@ require "test_helper"
 describe Work do
   let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  describe "validations" do
+    it "rejects validations with bad data" do
+      work
+      expect(work.valid?).must_equal false
+      expect(work.errors.messages[:title][0]).must_equal "can't be blank"
+      expect(work.errors.messages[:category][0]).must_equal "can't be blank"
+    end
+
+    it "passes validations with good data" do
+      work = works(:custom1)
+      expect(work).must_be :valid?
+    end
   end
 end
