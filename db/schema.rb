d956f3d2434d4998c97b6ff5a1995bcc6bbadbca
Kate Sandler <kate@katekatekate.net>
2019-04-24 10:41:33 -0700

    Migration for date_voted column in votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_004718) do
+ActiveRecord::Schema.define(version: 2019_04_24_174048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_004718) do
     t.integer "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date_voted"
   end
 
   create_table "works", force: :cascade do |t|
