b1119b0c6651ecb575fb588c9e9179165fb136a3
Sopheary Chiv <sophearychiv@gmail.com>
2019-04-23 21:45:04 -0700

    fixing error in heroku

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,10 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
+  # def top_10_media_by_category(category)
+  #   @all_media_in_category = Work.where(category: category)
+  # end
+
   def show
     @work = Work.find_by(id: params[:id])
     if !@work
