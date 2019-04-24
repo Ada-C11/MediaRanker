885c52db76ca343eb3064d831f52f3abb2c13c4f
amyesh <amyesh08@gmail.com>
2019-04-23 21:35:40 -0700

    added methods for new and create to works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -18,8 +18,63 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new(title: "Default Title", category: "album")
   end
 
   def create
+    @work = Work.new(work_params)
+
+    is_successful = @work.save
+
+    if is_successful
+      flash[:success] = "work added successfully"
+      redirect_to work_path(@work.id)
+    else
+      @work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+
+      render :new, status: :bad_request
+    end
+  end
+
+  #   def edit
+  #     @work = Book.find_by(id: params[:id])
+  #   end
+
+  #   def update
+  #     book = Book.find_by(id: params[:id])
+
+  #     is_successful = book.update(book_params)
+
+  #     if is_successful
+  #       flash[:success] = "book updated successfully"
+  #       redirect_to book_path(book.id)
+  #     else
+  #       @book = book
+  #       @book.errors.messages.each do |field, messages|
+  #         flash.now[field] = messages
+  #       end
+  #       render :edit, status: :bad_request
+  #     end
+  #   end
+
+  #   def destroy
+  #     book = Book.find_by(id: params[:id])
+
+  #     if book.nil?
+  #       flash[:error] = "That book does not exist"
+  #       redirect_to books_path
+  #     else
+  #       book.destroy
+  #       flash[:success] = "#{book.title} deleted"
+  #       redirect_to books_path
+  #     end
+  #   end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :creator, :description, :category, :publication_year)
   end
 end
