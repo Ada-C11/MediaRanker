79662a138f3931b99febab31ce538513ddb49d87
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-23 21:03:58 -0700

    added login form action, view, and test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "login_form" do
+    it "should get login form" do
+      get login_path
+
+      must_respond_with :success
+    end 
+  end 
 end
