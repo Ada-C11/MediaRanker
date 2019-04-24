2eda1f5bcd52b81b639b5d07823eff2e5df1fef8
Amy Phung <phung.amy85@gmail.com>
2019-04-23 19:30:51 -0700

    add top 10 and spotlight on homepage

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,17 @@
 class Work < ApplicationRecord
   has_many :votes
   
+  def self.albums
+    return self.where(category: "album")
+  end
+  
+  def self.books
+    return self.where(category: "book")
+  end
+  
+  def self.movies
+    return self.where(category: "movie")
+  end
   # def total_votes
     
   # end
