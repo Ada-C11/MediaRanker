4f3c55554c6c7e4c24f1c6b9bebf5e441cb7eee9
Kate Sandler <kate@katekatekate.net>
2019-04-24 10:37:59 -0700

    Fixes busted sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
-
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   get 'sessions/login', to: 'sessions#new'
   post 'sessions/login', to: 'sessions#login', as: 'login'
   delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
 
-  get 'users/index', to: 'users#index', as: 'users'
-  get 'users/show', to: 'users#show', as: 'user'
-
-  root 'landing#index'
   post 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
-
   resources :works
+
   resources :users, only: [:index, :show, :new]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
