dca21c507505abdf838f7078bd7a42e2027a0044
Grace Shea <grace.m.shea@gmail.com>
2019-04-24 09:30:18 -0700

    refinement to login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,16 @@ class UsersController < ApplicationController
   def login
     username = params[:user][:username]
     user = User.find_by(username: username)
+    if user.nil?
+      flash_msg = "Welcome new user!"
+    else
+      flash_msg = "Welcome back, #{username}!"
+    end
+
     user ||= User.create(username: username)
 
     session[:user_id] = user.id
-    flash[:success] = "#{username} is logged in."
+    flash[:success] = flash_msg
     redirect_to root_path
   end
 end
