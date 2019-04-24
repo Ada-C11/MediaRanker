8904db124d5a7102746516dc1b93c6332d0fc90d
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-24 09:55:56 -0700

    Added work/model tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,6 @@
 class Work < ApplicationRecord
   validates :category, presence: true
   validates :title, presence: true, uniqueness: true
-  validates :creator, presence: true
-  validates :description, presence: true, uniqueness: true
-  validates :publication_year, presence: true
+  
+  has_many :votes
 end
