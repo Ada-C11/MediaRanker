648b18df6a7f842933d02e13da4d38f3a3c797f0
kateannnichols <kateannnichols@gmail.com>
2019-04-24 09:53:03 -0700

    update works controller methods with controller filters, in progress

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,55 +3,56 @@
 class WorksController < ApplicationController
   before_action :find_work, except: %i[index new create]
 
-    def index
-        @work = Work.ordered
-    end
-
-    def new
-        @work = Work.new
-    end
-
-    def create
-        @work = Work.new(work_params[:work])
-        if @work.save
-        render :show
-    else
-        render :new
-    end
+  def index
+    @work = Work.ordered
   end
 
-    def edit
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params[:work])
+    if @work.save
+      render :show
+    else
+      render :new
     end
 
-    def show
-    end
+  def show; end 
 
-    def update
+  def show; end 
+
+  def update
     @work.update(work_params[:work])
     if @work.save
       redirect_to work_path(@work.id)
     else
-      render :edit
+      render :edit, status: :bad_request
     end
+end
+
+  def upvote
+    Work.upvote(@work)
+    redirect_to work_path(@work.id)
   end
 
-    def upvote
-        Work.upvote(@work)
-        redirect_to work_path(@work.id)
-    end
-
-    def destroy
-        @move.destroy
-        redirect_to work_path
-    end
+  def destroy
+    @move.destroy
+    redirect_to work_path
+  end
 
     private
 
-    def find_work
-        @work = Work.find(params[:id])
-    end
+  def find_work
+    @work = Work.find_by(id: params[:id])
 
-    def work_params
-        params.permit(work: %i[title creator publication_year])
-    end
+    unless @work 
+
   end
+
+  def work_params
+    params.permit(work: %i[title creator publication_year])
+  end
+end
+
