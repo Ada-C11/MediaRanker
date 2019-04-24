0cf6ae5b5be913ef47ccc965bd99de8837a0b0b0
Grace Shea <grace.m.shea@gmail.com>
2019-04-24 09:26:23 -0700

    login method added to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    user ||= User.create(username: username)
+
+    session[:user_id] = user.id
+    flash[:success] = "#{username} is logged in."
+    redirect_to root_path
+  end
 end
