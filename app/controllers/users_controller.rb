513d787d168adb120e2b8169a794e1eb47604855
Chantal Demissie <chantaldemissie@yahoo.com>
2019-04-23 21:25:59 -0700

    skeleton added to controllers. model relationships created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+  def index
+    #codehere
+  end
+
+  def new
+    #codehere
+  end
+
+  def edit
+    #codehere
+  end
+
+  def show
+    #codehere
+  end
+
+  def delete
+    #codehere
+  end
+
+  def login
+    #codehere
+  end
+
+  #logout
+  def destroy
+    #codehere
+  end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,26 @@
 class WorksController < ApplicationController
+
+  def index
+    #codehere
+  end
+
+  def new
+    #codehere
+  end
+
+
+  def edit
+    #codehere
+  end
+
+
+  def show
+    #codehere
+  end
+
+
+  def delete
+    #codehere
+  end
+
 end
