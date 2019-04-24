23a5ac27f290b78dc6cc428caf78a20443330fbc
amyesh <amyesh08@gmail.com>
2019-04-24 10:03:53 -0700

    updated login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user.nil?
+      user = User.create(username: params[:user][:username])
+      flash[:alert] = "#{user.username} logged in"
+      session[:user_id] = user.id
+    else
+      session[:user_id] = user.id
+      flash[:error] = "Unable to log in"
+    end
+    redirect_to root_path
+  end
+
   def show
   end
 
