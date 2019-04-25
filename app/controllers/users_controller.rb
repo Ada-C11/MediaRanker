4af803ea646706232e70c366f73988dd7d6fc9da
Bitaman <b.amani.s@gmail.com>
2019-04-25 14:13:39 -0700

    made the vote model and controller and added the work and user foreign key to votes table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,45 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-
   def login_form
     @user = User.new
   end
 
   def login
     username = params[:user][:username]
-    user = User.find_by(username: username)
-    user ||= User.create(username: username)
-    session[:user_id] = user.id
-    flash[:success] = "Successfully logged in as new user #{username}"
-    redirect_to root_path
-  end
+    @user = User.find_by(username: username)
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      @user = User.create(username: username)
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
 
-  def logout
-    session[:user_id] = nil
-    flash[:success] = "Successfully logged out"
-
-    redirect_to root_path
+    redirect_to works_path
   end
 
   def current
     @current_user = User.find_by(id: session[:user_id])
     unless @current_user
       flash[:error] = "You must be logged in to see this page"
-      redirect_to login_path
-      return
+      redirect_to root_path
     end
+  end
+
+  def logout
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = nil
+      redirect_to root_path
+    else
+      puts "No user is logged in"
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:user).permit(:username)
   end
 end
