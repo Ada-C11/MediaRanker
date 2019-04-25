3222005eaf5c67c827a91aff20cdbede9b8fa259
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-25 14:07:34 -0700

    voting logic to votes controller and model

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,17 +1,21 @@
 class VotesController < ApplicationController
   def create
     @vote = Vote.new
+    work_id = params[:work_id]
     if session[:user_id]
       user_id = session[:user_id]
       @vote.user_id = user_id
       @vote.work_id = params[:work_id]
-      check_unique_work(user_id, work_id)
-      work_id = params[:work_id]
-      p @vote.work_id
-      @vote.save
-      redirect_to works_path
+      if check_unique_work(user_id, work_id)
+        @vote.save
+        redirect_to works_path
+      else
+        flash[:status] = :error
+        flash[:message] = "user: has already voted for this work"
+      end
     else
-
+      flash[:status] = :error
+      flash[:message] = "A problem occurred: You must log in to do that."
     end
   end
 
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,14 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
+
+  def check_unique_work(user_id, work_id)
+    user = User.find(user_id)
+    vote = Vote.find_by(user: user_id)
+    if vote.work_id == work_id
+      return false
+    else
+      return true
+    end
+  end
 end
