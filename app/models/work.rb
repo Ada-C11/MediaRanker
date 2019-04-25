3d16313cb29d0ba8438876db7c551e4ca6918551
Bitaman <b.amani.s@gmail.com>
2019-04-25 16:45:46 -0700

    created the vote and added the number of votes for each work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,10 @@
 class Work < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: true
+
+  # def vote_count
+  #   @work = Work.find_by(id: params[:id])
+  #   @votes = Vote.find_by(work_id: @work.id)
+  #   return @votes.length
+  # end
 end
