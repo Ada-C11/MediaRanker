449b6c677a558d6b04a6df41126d1fbc8613b3b9
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-25 14:26:25 -0700

    work index and show tests added

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -2,13 +2,30 @@ require "test_helper"
 
 describe WorksController do
   it "should get index" do
-    get works_index_url
-    value(response).must_be :success?
+    get works_url
+    value(response).must_be :successful?
   end
 
-  it "should get show" do
-    get works_show_url
-    value(response).must_be :success?
+  describe "show" do
+    it "should get show" do
+      valid_work_id = works(:one).id
+
+      get works_url(valid_work_id)
+      value(response).must_be :successful?
+    end
+
+    it "should give a flash notice instead if invalid work" do
+      invalid_id = works(:one).id
+      works(:one).destroy
+
+      get work_path(invalid_id)
+
+      must_respond_with :redirect
+      expect(flash[:error]).must_equal "Unknown work"
+    end
+  end 
+
+  describe "create" do
   end
 
 end
