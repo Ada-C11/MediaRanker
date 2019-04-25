759b69073e8b459eea685f6e97620221dd5f763b
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-25 15:17:57 -0700

    migration to remove column media_id from votes model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_215323) do
+ActiveRecord::Schema.define(version: 2019_04_25_221329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_04_25_215323) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "media_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
