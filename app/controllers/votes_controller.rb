3b0bae5d03be767be368d1a030de147d8c5e1c14
Margaret Finnan <msfinnan@gmail.com>
2019-04-24 18:12:05 -0700

    started working on votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,7 @@
 class VotesController < ApplicationController
   def upvote
+    @vote = Vote.new
+    @vote.users_id = User.find_by(session[:user_id])
+    @vote.works_id = Work.find_by(id: params[:id])
   end
 end
