6a1ad1e2cdd7574c9cf7158ed8927887cb6aefa9
Margaret Finnan <msfinnan@gmail.com>
2019-04-24 17:39:26 -0700

    added route for upvotes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   resources :works
 
+  post "/upvote", to: "votes#upvote", as: "upvote"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
