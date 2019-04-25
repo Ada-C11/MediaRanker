7f14881e4ca1304c3ca66a969383c25e98ab5498
Chantal Demissie <chantaldemissie@yahoo.com>
2019-04-25 10:23:08 -0700

    added  controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,78 @@
 class UsersController < ApplicationController
+  # t.string :name
+  # t.date :join_date
 
   def index
-    #codehere
+    @users = User.all
   end
 
   def new
-    #codehere
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "User was successfully created."
+    else
+      render :new
+    end
   end
 
   def edit
-    #codehere
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "User was successfully updated."
+    else
+      render :new
+    end
   end
 
   def show
-    #codehere
+      @user = User.find(params[:id])
   end
 
-  def delete
-    #codehere
+  def login_form
+    @user = User.new
   end
 
   def login
-    #codehere
+    username = params[:user][:username]
+
+    user = User.find_by(username: username)
+    unless user
+      user = User.create(username: username)
+    end
+    session[:user_id] = user.id
+
+    flash[:status] = :sucess
+    flash[:message] = "Sucessfully logged in as user #{user.username}"
+    redirect_to root_path #homepage
   end
 
-  #logout
-  def destroy
-    #codehere
+  def current
+    @user = User.find_by(id: session[:user_id])
+
+    unless @user
+      flash[:status] = :error
+      flash[:message] = "you must be logged in to see this page"
+      redirect_to login_path
+      return
+    end
   end
+
+  def logout
+    session[:user_id] = nil
+
+    flash[:status] = :success
+    flash[:message] = "Sucessfully logged out"
+
+    redirect_to root_path
+  end
+
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,26 +1,61 @@
 class WorksController < ApplicationController
+  # t.string :category
+  # t.string :title
+  # t.string :creator
+  # t.date :publication_year
+  # t.string :description
 
   def index
-    #codehere
+    @works_by_category = Work.all.group_by{ |work| work.category }
   end
 
   def new
-    #codehere
+    @work = Work.new
   end
 
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to @work, notice: "Work was successfully created."
+    else
+      render :new
+    end
+  end
 
   def edit
-    #codehere
+      @work = Work.find(params[:id])
   end
 
+  def update
+    @work = Work.find(params[:id])
+    if @work.update(work_params)
+      redirect_to @work, notice: "Work was successfully updated."
+    else
+      render :new
+    end
+  end
 
   def show
-    #codehere
+      @work = Work.find(params[:id])
   end
 
+  def destroy
+    work_id = params[:id]
 
-  def delete
-    #codehere
+    work = Work.find_by(id: work_id)
+
+    unless work
+      head :not_found
+      return
+    end
+
+    work.destroy
+
+    redirect_to works_path
+  end
+
+  def upvote
   end
 
 end
