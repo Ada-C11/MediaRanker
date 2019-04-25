a6cef6b204eec558395536bff0ab3486861c6ec3
Shubha Rajan <shubharajan94@gmail.com>
2019-04-25 15:08:48 -0700

    added error_list view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def error_list(model)
+    error_list = ""
+    if model && model.errors
+      error_list += "<ul>"
+      model.errors.messages.each do |column, error|
+        error.each do |message|
+          error_list += "<li>#{column}: #{message}</li>"
+        end
+      end
+      error_list += "</ul>"
+    end
+    return error_list.html_safe
+  end
 end
