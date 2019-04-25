2ebc58d2706bb074add2463a2649e4f8b93d4c17
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-25 10:59:52 -0700

    work model updated for title, validations added

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,13 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
+
+    if !@work
+      head :not_found
+    end
   end
 end
