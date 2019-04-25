2fc7e8b47f6570775c982253db436283913c673e
Tatiana Quintana <tatsqui@gmail.com>
2019-04-25 10:22:07 -0700

    testing helper method for user routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   resources :works
   # will need a nested route for upvotes
 
-  # routes for user login
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
-  get "/users/current", to: "users#current", as: "current_user"
+  # helper method for user routes
+  user_routes
 end
