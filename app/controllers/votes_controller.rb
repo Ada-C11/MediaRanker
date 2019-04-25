f3916a4d2c49cd2c38dbb5d404f547057f4963e6
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-25 13:16:43 -0700

    create and vote params method in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+
+    def create
+        vote = Vote.create(vote_params)
+    end
+
+    def vote_params
+        params.require(:vote).permit(:user_id, :work_id)
+    end
 end
