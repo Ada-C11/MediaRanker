370e1de34c27631ee2551c445de891011d0f426c
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-24 21:13:49 -0700

    added testing for failur to login a user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -62,11 +62,24 @@ describe UsersController do
 
       user = User.find_by(username: login_data[:user][:username])
 
-      # Verify the user ID was saved - if that didn't work, this test is invalid
       expect(flash[:success]).must_equal "Successfully created new user #{user.username} with ID #{user.id}!"
+      expect(session[:user_id]).must_equal user.id
       must_respond_with :redirect
       must_redirect_to root_path
-      expect(session[:user_id]).must_equal user.id
+    end
+
+    it "responds accordingly when a user can't be logged in" do
+      login_data = {
+        user: {
+          username: "",
+        },
+      }
+
+      expect {
+        post login_path, params: login_data
+      }.wont_change "User.count"
+
+      must_respond_with :bad_request
     end
   end
 end
