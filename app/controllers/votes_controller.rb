9f8322614689501bf0a82ff7c2978883cc6f3eb2
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-25 13:53:27 -0700

    changed work show file to have upvote button. debugged vote create bug

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,21 @@
 class VotesController < ApplicationController
+  def create
+    @vote = Vote.new
+    if session[:user_id]
+      user_id = session[:user_id]
+      @vote.user_id = user_id
+      @vote.work_id = params[:work_id]
+      check_unique_work(user_id, work_id)
+      work_id = params[:work_id]
+      p @vote.work_id
+      @vote.save
+      redirect_to works_path
+    else
 
-    def create
-        vote = Vote.create(vote_params)
     end
+  end
 
-    def vote_params
-        params.require(:vote).permit(:user_id, :work_id)
-    end
+  def vote_params
+    params.require(:vote).permit(:user_id, :work_id)
+  end
 end
