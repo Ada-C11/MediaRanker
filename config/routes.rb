60393fb9c855ebfa089209791fc1fbf2d58266c6
Tatiana Quintana <tatsqui@gmail.com>
2019-04-25 10:20:36 -0700

    g controller Users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root "homepages#index"
 
   resources :works
-  
   # will need a nested route for upvotes
 
+  # routes for user login
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
