81b88cadd77d493c767def0c699e39c120caa2e6
Angela <angela.ohh@gmail.com>
2019-04-24 21:04:45 -0700

    added user and work id to vote table

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
   get "/users/current", to: "users#current", as: "current_user"
+
+  ### ROUTES FOR COUNTING VOTES
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
