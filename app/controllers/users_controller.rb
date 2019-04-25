ab4e1e642f4134f4369f618a496887325d563dc1
Heather Izumi <hkizumi@gmail.com>
2019-04-25 10:25:26 -0700

    Added Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:current, :logout]
+
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    user = User.find_by(username: params[:user][:username])
+    if user.nil?
+      user = User.create(username: username)
+    end
+
+    if @user.id
+      session[:user_id] = user.id
+      flash[:alert] = "Successfully logged in as existing user #{user.username}"
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to log in"
+      redirect_to root_path
+    end
+  end
+
+  def current
+    # @user = User.find_by(id: session[:user_id])
+    if @user.nil?
+      flash[:error] = "You must log in to do that"
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    # user = User.find_by(id: session[:user_id])
+    session[:user_id] = nil
+    flash[:notice] = "Logged out #{user.username}"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :works
   resources :homepages, only: [:index]
 
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
