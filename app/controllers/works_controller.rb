0a883144589cac4658800b42cbdd4e3540fece61
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-04-25 12:21:38 -0700

    work user and create functions added

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -10,4 +10,30 @@ class WorksController < ApplicationController
       head :not_found
     end
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.new(work_params)
+
+    is_successful = work.save
+
+    if is_successful
+      flash[:success] = "Work added successfully"
+      redirect_to work_path(work.id)
+    else
+      work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :created_by, :published, :description)
+  end
 end
