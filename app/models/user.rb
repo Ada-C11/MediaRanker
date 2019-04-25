65a0d8cd586975bf90e2758e36e4a3bb80b2d1a4
Angela <angela.ohh@gmail.com>
2019-04-25 14:38:53 -0700

    added table for user show view that includes vote info

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
 
-  #   def vote_count
-  #     self.votes.length
-  #   end
+  def vote_count
+    self.votes.length
+  end
 end
