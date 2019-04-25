207fdd55c048327afd92368b2590895083bf2302
Margaret Finnan <msfinnan@gmail.com>
2019-04-25 16:59:20 -0700

    turned functionality for user to only vote on a work once back on

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,22 +2,21 @@ class VotesController < ApplicationController
   def create
     @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
 
-    # if Vote.find_by(user_id: session[:user_id], work_id: params[:work_id]) != nil
-    #   flash[:failure] = "You cannot vote on a work twice"
-    # else
-    is_successful = @vote.save
-
-    if is_successful
-      flash[:success] = "Upvoted!"
-    elsif session[:user_id].nil?
-      flash[:failure] = "You must be logged in to upvote a work"
+    if Vote.find_by(user_id: session[:user_id], work_id: params[:work_id]) != nil
+      flash[:failure] = "You cannot vote on a work twice"
     else
-      @vote.errors.messages.each do |field, messages|
-        flash[field] = messages
+      is_successful = @vote.save
+
+      if is_successful
+        flash[:success] = "Upvoted!"
+      elsif session[:user_id].nil?
+        flash[:failure] = "You must be logged in to upvote a work"
+      else
+        @vote.errors.messages.each do |field, messages|
+          flash[field] = messages
+        end
       end
     end
+    redirect_to work_path(params[:work_id])
   end
-
-  redirect_to work_path(params[:work_id])
-  # end
 end
