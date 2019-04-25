645e1de3ea7a84a2c1ae770d202fbc03a8f8dea3
Heather Izumi <hkizumi@gmail.com>
2019-04-24 21:19:49 -0700

    Work model tests for validation

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,36 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:album) { works(:album) }
 
   it "must be valid" do
-    value(work).must_be :valid?
+    expect(album.valid?).must_equal true
+  end
+
+  describe "validations" do
+    it "requires a category" do
+      album.category = nil
+
+      valid_work = album.valid?
+
+      expect(valid_work).must_equal false
+      expect(album.errors.messages).must_include :category
+      expect(album.errors.messages[:category]).must_equal ["can't be blank"]
+    end
+
+    it "requires a unique title" do
+      duplicate_album = Work.new(category: album.category, title: album.title)
+
+      expect(duplicate_album.save).must_equal false
+
+      expect(duplicate_album.errors.messages).must_include :title
+      expect(duplicate_album.errors.messages[:title]).must_equal ["has already been taken"]
+    end
+  end
+
+  describe "relationships" do
+  end
+
+  describe "custom methods" do
   end
 end
