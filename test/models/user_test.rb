d58cf71f4c894e39a53a1b9c191bad8c554947b9
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-25 15:06:22 -0700

    added validation and validation test for username

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { users(:user_one) }
 
   it "must be valid" do
-    value(user).must_be :valid?
+    valid_user = user.valid?
+
+    expect(valid_user).must_equal true
+  end
+
+  it "requires a username" do
+    user.username = nil
+
+    expect(user.valid?).must_equal false
+    expect(user.errors.messages).must_include :username
+    expect(user.errors.messages[:username]).must_equal ["can't be blank"]
   end
 end
