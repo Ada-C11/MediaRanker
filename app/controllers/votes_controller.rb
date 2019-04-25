f5bd614bfc1c9c921e9ca6c7f886efbf9a87c699
Margaret Finnan <msfinnan@gmail.com>
2019-04-25 14:44:27 -0700

    voting method complete in votes controller, can vote from show page and index page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,22 @@
 class VotesController < ApplicationController
   def create
-    @vote = Vote.new
+    # if session[:user_id] == nil
+    #   flash[:failure] = "You must be logged in to upvote a work"
+    # else
 
-    @vote.user_id = session[:user_id]
-    @vote.work_id = params[:work_id]
+    @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
 
     is_successful = @vote.save
 
     if is_successful
       flash[:success] = "Upvoted!"
-      redirect_to work_path(params[:work_id])
+    elsif session[:user_id].nil?
+      flash[:failure] = "You must be logged in to upvote a work"
     else
       @vote.errors.messages.each do |field, messages|
-        flash.now[field] = messages
+        flash[field] = messages
       end
-      render :new, status: :bad_request
     end
+    redirect_to work_path(params[:work_id])
   end
 end
