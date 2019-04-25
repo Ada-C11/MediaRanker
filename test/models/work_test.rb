a52a01b95203ca197eb7102faa54e9775e716374
sjscotton <sj.scotton@gmail.com>
2019-04-24 19:24:42 -0700

    styled work from

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -17,4 +17,20 @@ describe Work do
       expect(result).must_equal false
     end
   end
+
+  describe "self.get_media_catagories" do
+    it "returns an array of length 3" do
+      media = Work.get_media_catagories
+
+      expect(media).must_be_instance_of Array
+
+      expect(media.length).must_equal 3
+    end
+  end
+
+  describe "self.top_media" do
+  end
+
+  describe "self.spotlight" do
+  end
 end
