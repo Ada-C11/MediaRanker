42c98da9c671b33c2ecd67d086a87edc0876d768
Shirley Chu <shirley.cchu@gmail.com>
2019-04-24 20:53:21 -0700

    finished restful actions, edit and new forms

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,6 @@
 class WorksController < ApplicationController
+  before_action :find_work, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Work.books
     @albums = Work.albums
@@ -10,14 +12,6 @@ class WorksController < ApplicationController
     @top_books = Work.top_ten_books
   end
 
-  def show
-    work_id = params[:id]
-    @work = Work.find_by(id: work_id)
-    unless @work
-      head :not_found
-    end
-  end
-
   def new
     @work = Work.new
   end
@@ -25,13 +19,42 @@ class WorksController < ApplicationController
   def create
     @work = Work.new(work_params)
     if @work.save
-      redirect_to work_path
+      flash[:status] = :success
+      flash[:message] = "Successfully created #{@work.category} #{@work.id}"
+      redirect_to work_path(@work)
     else
       render :new, status: :bad_request
     end
   end
 
+  def update
+    if @work.save(work_params)
+      flash[:status] = :success
+      flash[:message] = "Successfully updated #{@work.category} #{@work.id}"
+      redirect_to work_path(@work)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  def destroy
+    @work.delete
+    flash[:status] = :success
+    flash[:message] = "Successfully deleted #{@work.category} #{@work.id}"
+    redirect_to works_path
+  end
+
+  private
+
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :publication_year)
+  end
+
+  def find_work
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    unless @work
+      head :not_found
+    end
   end
 end
