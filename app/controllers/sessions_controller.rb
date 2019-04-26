9c9c543b966f7555c0c6f3610cea3ecba30162dd
Kate Sandler <kate@katekatekate.net>
2019-04-25 23:39:10 -0700

    Refactor login method, clean up all the things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,24 @@
 class SessionsController < ApplicationController
   def login
-    @user = User.find_by(username: params[:user][:username])
-    if @user
-      flash[:success] = "Welcome back, #{@user.username}!"
-      redirect_to root_path
+    username = params[:username]
+    if !username.nil? && user = User.find_by(username: username)
+      flash[:success] = "Welcome back, #{user.username}!"
     else
-      @user = User.create(username: params[:user][:username], date_joined: DateTime.now)
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:success] = "New user created: #{@user.username} with ID #{@user.id}"
-        redirect_to root_path
-        else
+      user = User.new(username: username)
+      if user.save
+        session[:user_id] = user.id
+        flash[:success] = "New user created: #{user.username} with ID #{user.id}"
+      else
         flash.now[:failure] = "Login failed"
         render :new
       end
     end
+    redirect_to root_path
   end
 
-  def destroy
+  def logout
     session[:user_id] = nil
     flash[:success] = "You have logged out."
-  end
-
-  def new
-    @user = User.new
+  redirect_to root_path
   end
 end
