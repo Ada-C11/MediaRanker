604936fde54b82ca4e3478731ebf8d602ae09080
Erica Norris <norrise120@gmail.com>
2019-04-26 11:13:38 -0700

    Did work for works index page--added Works controller, updated routes, and added html

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,7 @@
+class WorksController < ApplicationController
+  def index
+    @books = Work.where(category: "book")
+    @albums = Work.where(category: "album")
+    @movies = Work.where(category: "movie")
+  end
+end
