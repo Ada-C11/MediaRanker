dce2868905332dc036c24b11cb2ec07080b50474
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-26 11:02:29 -0700

    login as username button on nav bar, font for body changed to Gudea, model method that returns user associated with session user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates :username, presence: true
   has_many :votes
+
+  def self.user(user_id)
+    user = User.find_by(id: user_id)
+    return user
+  end
 end
