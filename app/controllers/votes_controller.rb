92210169ded5b00f1206c07d1879292d88ba3dc2
Mello-Cello <celloelle@gmail.com>
2019-04-25 18:57:11 -0700

    Added strong params to all three controllers.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,13 @@
 class VotesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  private
+
+  def work_params
+    return params.require(:vote).permit(:user_id, :work_id)
+  end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,28 @@
 class WorksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
