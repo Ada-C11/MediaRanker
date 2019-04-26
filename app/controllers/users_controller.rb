825acbc3994a4742d8d8e4c0e5741b7cb9fc92dd
Tatiana Quintana <tatsqui@gmail.com>
2019-04-26 16:23:53 -0700

    added more functionality to login/logout added logout action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,16 @@ class UsersController < ApplicationController
   def login
     username = params[:user][:username]
     @user = User.find_by(username: username)
-    
+
     if @user
       session[:user_id] = @user.id
-      session[:username] = username
+      session[:username] = @user.username
       flash[:status] = :success
       flash[:message] = "Successfully logged in as #{username}"
     else
       @user = User.create!(username: params[:user][:username])
       session[:user_id] = @user.id
-      session[:username] = username
+      session[:username] = @user.username
       flash[:status] = :success
       flash[:message] = "Successfully logged in as #{username}"
     end
@@ -31,5 +31,14 @@ class UsersController < ApplicationController
       head :not_found
       return
     end
+  end
+
+  def logout
+    session[:user_id] = nil
+    session[:username] = nil
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to root_path
   end
 end
