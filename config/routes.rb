f35c3960bcb01d52a6d9cbf1f3fb2ee66472fa9d
Kate Sandler <kate@katekatekate.net>
2019-04-25 22:56:27 -0700

    Remove vestigial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root 'landing#index'
 
-  get 'sessions/login', to: 'sessions#new'
-  post 'sessions/login', to: 'sessions#login', as: 'login'
-  delete 'sessions/logout', to: 'sessions#destroy'
+  get 'sessions/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#login'
   post 'sessions/logout', to: 'sessions#destroy', as: 'logout'
 
   post 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
   resources :works
 
-  resources :users, only: [:index, :show, :new]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
