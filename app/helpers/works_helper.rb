9e97cc29a8843f94864cfa2940e0ba420a5a789d
Shubha Rajan <shubharajan94@gmail.com>
2019-04-25 18:54:43 -0700

    index displays vote count

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -1,14 +1,15 @@
 module WorksHelper
   def list_works(category)
     works_rows = ""
-    works_in_category_sorted = Work.where(category: category).sort_by { |work| work.votes.count }
+    works_in_category_sorted = Work.where(category: category).sort_by { |work| work.votes.count }.reverse
     works_in_category_sorted.each do |work|
       works_rows += "<tr>" +
+                    "<td>#{work.votes.count}</td>" +
                     "<td>#{link_to work.title, work_path(work.id)}</td>" +
                     "<td>#{work.creator}</td>" +
                     "<td>#{work.publication_year}</td>" +
-                    "<td>#{link_to "Upvote", work_votes_path(work_id: work.id), method: :post}</td>"
-      "</tr>"
+                    "<td>#{link_to "Upvote", work_votes_path(work_id: work.id), method: :post}</td>" +
+                    "</tr>"
     end
     return works_rows.html_safe
   end
