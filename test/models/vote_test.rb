ff5879e28cb2fa22b4762f4be063b2f163dcce83
Shubha Rajan <shubharajan94@gmail.com>
2019-04-25 20:11:57 -0700

    validation tests for votes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,14 +1,49 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) {
-    Vote.new(
+  before do
+    @vote = Vote.create(
       user_id: User.first.id,
       work_id: Work.first.id,
     )
-  }
+  end
+  describe "validations" do
+    it "is valid when the user has not voted on the work before" do
+      result = @vote.valid?
+      expect(result).must_equal true
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "is invalid if a user votes for the same work more than once" do
+      test_vote = Vote.new(
+        user_id: User.first.id,
+        work_id: Work.first.id,
+      )
+
+      result = test_vote.valid?
+
+      expect(result).must_equal false
+    end
+
+    it "is valid if the same user votes for a different work" do
+      test_vote = Vote.new(
+        user_id: User.first.id,
+        work_id: Work.last.id,
+      )
+
+      result = test_vote.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "is valid if a different user votes for the same work" do
+      test_vote = Vote.new(
+        user_id: User.last.id,
+        work_id: Work.first.id,
+      )
+
+      result = test_vote.valid?
+
+      expect(result).must_equal true
+    end
   end
 end
