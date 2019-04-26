39328e0a5e041d5bf6b34cef0b7631052020d931
Tatiana Quintana <tatsqui@gmail.com>
2019-04-26 15:36:56 -0700

    changed .list to .top_ten_list to better reflect method purpose

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,12 +6,22 @@ class Work < ApplicationRecord
   validates :title, presence: true
  
   # helper methods to return lists by category
-  def self.list(category)
-   return self.where(category: category)
+  def self.top_ten_list(category)
+    list = self.where(category: category)
+    sorted_list =list.sort_by { |media| media.upvotes.count }
+    return sorted_list.reverse!.first(10)
   end
 
   # logic for top ten votes
-  def self.top_ten(category)
-    return self.where(category: category).max_by { |work| work.votes.length }
+  # def self.top_ten(category)
+  #   return self.where(category: category).max_by { |work| work.upvotes.length }
+  # end
+
+  def self.spotlight
+    max_votes = self.all.max_by { | work |
+     work.votes.length     
+    }  
+
+    return max_votes
   end
 end
