7df1690d360028039f4f179f567bc077952aa6da
Kate Sandler <kate@katekatekate.net>
2019-04-25 20:20:59 -0700

    Changes routes to fix login bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'landing#index'
-  resources :works
-
-  resources :users, only: [:index, :show, :new]
 
   get 'sessions/login', to: 'sessions#new'
   post 'sessions/login', to: 'sessions#login', as: 'login'
-  post '/logout', to: 'sessions#logout', as: 'logout'
+  delete 'sessions/logout', to: 'sessions#destroy'
+  post 'sessions/logout', to: 'sessions#destroy', as: 'logout'
 
   post 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
+  resources :works
+
+  resources :users, only: [:index, :show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
