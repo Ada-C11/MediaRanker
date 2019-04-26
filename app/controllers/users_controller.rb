8db6a343df2d22c2a4872015380f26efca0adde7
Jessica <jessica.homet@gmail.com>
2019-04-26 10:53:33 -0700

    Fill user controller login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationCont
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+    user = User.create(name: name) if user.nil?
+
+    if user.id
+      session[:user_id] = user.id
+      flash[:alert] = "#{user.name} logged in"
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to log in"
+      redirect_to root_path
+    end
+  end
+
+  def current
+    @user = User.find_by(id: session[:user_id])
+    if @user.nil?
+      flash[:error] = "You must be logged in first!"
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    user = User.find_by(id: session[:user_id])
+    session[:user_id] = nil
+    flash[:notice] = "Logged out #{user.name}"
+    redirect_to root_path
+  end
+
   def index
   end
 
