8b368d04609c11b533c9d3b1cee23ee104464266
Erica Norris <norrise120@gmail.com>
2019-04-26 14:10:44 -0700

    Created create method and route for Work--need to update redirect in create after details page is created

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -8,4 +8,25 @@ class WorksController < ApplicationController
   def new
     @work = Work.new
   end
+
+  def create
+    @work = Work.new(work_params)
+
+    successful = @work.save
+    if successful
+      flash[:status] = :success
+      flash[:message] = "Successfully created #{@work.category} #{@work.id}"
+      redirect_to root_path #update to details page once created!
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "A problem occurred: could not create #{@work.category}"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
+  end
 end
