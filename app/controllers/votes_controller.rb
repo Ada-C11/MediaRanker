5d989d8c8851ab35c53e831cf9fdb531f95375da
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-26 15:54:00 -0700

    added create action to Votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,8 @@
 class VotesController < ApplicationController
+  def create
+    # raise
+    @work = Work.find_by(id: params[:work_id])
+    @user = User.find_by(id: session[:user_id])
+    @work.users << @user
+  end
 end
