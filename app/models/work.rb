9d3acce0ab9314a1f75a2832d606985ac74147d8
Angela <angela.ohh@gmail.com>
2019-04-26 15:45:11 -0700

    added all work.rb tests for custom methods

--- app/models/work.rb
+++ app/models/work.rb
@@ -10,46 +10,23 @@ class Work < ApplicationRecord
   def self.spotlight_work(works)
     spotlight_work = works.first
     works.each do |work|
-      if work.vote_count == nil
-        return nil
-      elsif work.vote_count > spotlight_work.vote_count
+      if work.vote_counter > spotlight_work.vote_counter
         spotlight_work = work
       end
     end
+    return nil if spotlight_work.vote_counter == 0
     return spotlight_work
   end
 
-  def self.top_ten_books(works)
-    top_books = []
+  def self.top_ten(works, category)
+    top_works = []
     sorted_works = works.order(:vote_count).reverse
     sorted_works.each do |work|
-      if work.category == "book"
-        top_books << work
+      if work.category == category
+        top_works << work
       end
     end
 
-    return top_books[0..9]
-  end
-
-  def self.top_ten_albums(works)
-    top_albums = []
-    sorted_works = works.order(:vote_count).reverse
-    sorted_works.each do |work|
-      if work.category == "album"
-        top_albums << work
-      end
-    end
-    return top_albums[0..9]
-  end
-
-  def self.top_ten_movies(works)
-    top_movies = []
-    sorted_works = works.order(:vote_count).reverse
-    sorted_works.each do |work|
-      if work.category == "movie"
-        top_movies << work
-      end
-    end
-    return top_movies[0..9]
+    return top_works[0..9]
   end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 describe Work do
   let(:work) { works(:one) }
+  let(:popular_work) { works(:two) }
   let(:vote_one) { votes(:one) }
   let(:vote_two) { votes(:two) }
+  let(:vote_three) { votes(:two) }
 
   it "must be valid" do
     expect(work.valid?).must_equal true
@@ -40,7 +42,53 @@ describe Work do
   end
 
   describe "custom methods" do
-    it "" do
+    describe "vote_counter" do
+      it "returns the correct number of votes" do
+        work.votes << vote_one
+        work.votes << vote_two
+        expect(work.vote_counter).must_equal 2
+      end
+
+      it "returns 0 when no votes have been made" do
+        expect(work.vote_counter).must_equal 0
+      end
+    end
+
+    describe "spotlight_work" do
+      it "will return the highest voted work" do
+        popular_work.votes << vote_one
+        popular_work.votes << vote_two
+        work.votes << vote_three
+        works = [work, popular_work]
+
+        expect(Work.spotlight_work(works)).must_equal popular_work
+      end
+
+      it "will return nil if no one has voted yet" do
+        works = [work, popular_work]
+        expect(Work.spotlight_work(works)).must_equal nil
+      end
+    end
+
+    describe "top_ten" do
+      it "returns an array of ten works sorted by the most count by category - may include 0 votes" do
+        works = Work.all
+        category_one = works(:one).category
+        top_ten_works = Work.top_ten(works, category_one)
+
+        expect(top_ten_works.length).must_equal 10
+        expect(top_ten_works).must_include works(:ten)
+        expect(top_ten_works).wont_include works(:eleven)
+        expect(top_ten_works.first.category).must_equal category_one
+      end
+
+      it "returns the max number of works in category is there are less than 10 works" do
+        works = Work.all
+        category_two = works(:eleven).category
+        top_works = Work.top_ten(works, category_two)
+
+        expect(top_works.length).must_equal 1
+      end
     end
   end
 end
