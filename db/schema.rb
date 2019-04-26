f940d35598333b988ca8552fc92c45c48d4bcb24
Tatiana Quintana <tatsqui@gmail.com>
2019-04-25 19:13:14 -0700

    added Upvote, deleted garbage test from Work

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_170559) do
+ActiveRecord::Schema.define(version: 2019_04_26_021141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "upvotes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
