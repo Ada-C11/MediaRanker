55b235a1fbfd84a247237c02e3a0da4668f903a2
Erica Norris <norrise120@gmail.com>
2019-04-26 15:05:21 -0700

    Wrote tests for index, all passing, updated test_helper to fix bug

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
-describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+describe "WorksController" do
+  describe "index" do
+    it "renders without crashing" do
+      # Arrange
+
+      # Act
+      get works_path
+
+      # Assert
+      must_respond_with :ok
+    end
+
+    it "renders even if there are zero books" do
+      # Arrange
+      Work.destroy_all
+
+      # Act
+      get works_path
+
+      # Assert
+      must_respond_with :ok
+    end
+  end
 end
