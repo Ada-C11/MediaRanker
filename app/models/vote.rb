6eb02decaeeb6566dd98901f51c1da5de2717702
sjscotton <sj.scotton@gmail.com>
2019-04-26 15:11:12 -0700

    added validation tests for votes

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,9 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
+  validates :user_id, presence: true
+  validates :work_id, presence: true
+
   # def is_revote?(user_id, work_id)
   #   return Vote.find_by(user_id: user_id, work_id: work_id)
   # end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -6,4 +6,30 @@ describe Vote do
   # it "must be valid" do
   #   value(vote).must_be :valid?
   # end
+
+  describe "Validations" do
+    before do
+      @vote = Vote.new({ user_id: users.first.id, work_id: works.first.id })
+    end
+    it "Validates with good data" do
+      result = @vote.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "Wont validate a vote without a user_id" do
+      @vote.user_id = nil
+
+      result = @vote.valid?
+
+      expect(result).must_equal false
+    end
+    it "Wont validate a vote without a work_id" do
+      @vote.work_id = nil
+
+      result = @vote.valid?
+
+      expect(result).must_equal false
+    end
+  end
 end
