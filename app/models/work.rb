a3d5207ec3742975d1e3b3feea5d25c910044ea0
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-26 14:44:06 -0700

    added potential work model methods for top 10 and spotlight

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,15 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
+  def self.media_spotlight
+    all_works = Work.all
+    popular_works = all_works.sort_by { |work| work.votes.count }
+    return popular_works.first
+  end
+
+  def self.top_10(category)
+    work_with_category = Work.where(category: category)
+    popular_works = work_with_category.sort_by { |work| work.votes.count }
+    return popular_works[0..9]
+  end
 end
