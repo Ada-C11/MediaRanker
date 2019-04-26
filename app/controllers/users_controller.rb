25b1ff27e3d8c2e1ed18d2cd844a006559a78c18
Bitaman <b.amani.s@gmail.com>
2019-04-26 14:09:02 -0700

    tests for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,36 +5,38 @@ class UsersController < ApplicationController
 
   def login
     username = params[:user][:username]
-    @user = User.find_by(username: username)
-    if @user
-      session[:user_id] = @user.id
-      flash[:success] = "Successfully logged in as returning user #{username}"
-    else
-      @user = User.create(username: username)
-      session[:user_id] = @user.id
-      flash[:success] = "Successfully logged in as new user #{username}"
+
+    user = User.find_by(username: username)
+    unless user
+      user = User.create(username: username)
     end
 
-    redirect_to works_path
+    session[:user_id] = user.id
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged in as user #{user.username}"
+
+    redirect_to root_path
   end
 
   def current
-    @current_user = User.find_by(id: session[:user_id])
-    unless @current_user
-      flash[:error] = "You must be logged in to see this page"
-      redirect_to root_path
+    @user = User.find_by(id: session[:user_id])
+
+    unless @user
+      flash[:status] = :error
+      flash[:message] = "You must be logged in to vote"
+      redirect_to login_path
+      return
     end
   end
 
   def logout
-    username = params[:user][:username]
-    user = User.find_by(username: username)
-    if user
-      session[:user_id] = nil
-      redirect_to root_path
-    else
-      puts "No user is logged in"
-    end
+    session[:user_id] = nil
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to root_path
   end
 
   private
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,20 +7,31 @@ describe UsersController do
       must_respond_with :ok
     end
   end
-  describe "login" do
-    # it "can log in a user" do
-    #   user_data = {
-    #    user: {
-    #      username: "test user"
-    #    }
+  # describe "login" do
+  #   it "can make a session with the user that logged in" do
+  #     user_data = {
+  #       user: {
+  #         uesrname: "test user",
+  #       },
+  #     }
+  #     test_user = User.new(user_data)
+  #     perform_login(test_user)
+  #     expect(session[:username]).must_equal "test user"
+  #   end
+  # end
+  describe "current" do
+    before do
+      perform_login
+    end
+    it "returns 200 OK for a logged-in user" do
+      # Arrange
+      # perform_login
 
-    #    #post login_path, params: user_data
+      # Act
+      get current_user_path
 
-    #     must_respond_with :redirect
-    #     must_redirect_to works_path
-
-    #     expect(session[:username]).must_equal user_data[:user][username]
-    #   end
-
+      # Assert
+      must_respond_with :success
+    end
   end
 end
