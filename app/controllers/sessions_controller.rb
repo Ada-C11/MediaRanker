87f3a1ebf4b9133daff16b7e528309f38ffd60c9
Kate Sandler <kate@katekatekate.net>
2019-04-25 20:17:49 -0700

    Refactors flash messages for readability

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,38 +1,25 @@
 class SessionsController < ApplicationController
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Welcome back, #{user.username}!"
+    @user = User.find_by(username: params[:user][:username])
+    if @user
+      flash[:success] = "Welcome back, #{@user.username}!"
+      redirect_to root_path
     else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "New user created: #{user.username} with ID #{user.id}"
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Login failed"
-        flash.now[:messages] = user.errors.messages
-        return
+      @user = User.create(username: params[:user][:username], date_joined: DateTime.now)
+      if @user.save
+        session[:user_id] = @user.id
+        flash[:success] = "New user created: #{@user.username} with ID #{@user.id}"
+        redirect_to root_path
+        else
+        flash.now[:failure] = "Login failed"
+        render :new
       end
     end
-    redirect_to root_path
-  end
-
-  def logout
-    session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "You have logged out."
-    redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "You have logged out."
-    redirect_to root_path
+    flash[:success] = "You have logged out."
   end
 
   def new
