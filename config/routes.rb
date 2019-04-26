477467b165dc6e210c2ba71748600d6907a634c2
Jessica <jessica.homet@gmail.com>
2019-04-26 10:54:34 -0700

    Add login, logout, and current user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'homepages/index'
+  get "homepages/index"
   root to: "homepages#index" # where do i want this to be
   resources :users
   resources :works
   resources :votes, only: [:create] # maybe
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
