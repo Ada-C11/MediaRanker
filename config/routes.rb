3f62fe461c542243b4e4abdf360b89f91fc0788b
Mello-Cello <celloelle@gmail.com>
2019-04-25 18:44:32 -0700

    Generated controllers for all three models. Edited routes file to remove users#edit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :works
-  resources :users, except: [:update, :destroy]
+  resources :users, except: [:edit, :update, :destroy]
   resources :votes, only: [:index, :create]
 end
