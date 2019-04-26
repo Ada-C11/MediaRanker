58f5960b03c6b9e9e83fc47e7c560a7ba973c4b3
Amy Phung <phung.amy85@gmail.com>
2019-04-25 19:03:23 -0700

    remove grouping by category methods in Work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,28 +4,10 @@ class Work < ApplicationRecord
   validates :author, presence: true
   validates :category, presence: true
   
-  # def top_ten(array)
-  #   return array.max_by(10) { |work| work.votes.length }
-  # end
   
 
-def self.top_ten(category)
-  return Work.where(category: category).max_by(10) { |work| work.votes.length }
-end
+  def self.top_ten(category)
+    return Work.where(category: category).max_by(10) { |work| work.votes.length }
+  end
 
-  
-  def self.albums
-    return self.where(category: "album")
-  end
-  
-  def self.books
-    return self.where(category: "book")
-  end
-  
-  def self.movies
-    return self.where(category: "movie")
-  end
-  # def total_votes
-    
-  # end
 end
