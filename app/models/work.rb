669408d53f3e17984b45f981f710f30b9a59d4a8
Shubha Rajan <shubharajan94@gmail.com>
2019-04-25 18:14:18 -0700

    updated Work#top_ten to handle votes

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,9 +8,9 @@ class Work < ApplicationRecord
   def self.top_ten(category)
     works_in_category = self.where(category: category).sort_by { |work| work.votes.count }
     if works_in_category.length > 10
-      return works_in_category[0...10]
+      return works_in_category.reverse[0...10]
     else
-      return works_in_category
+      return works_in_category.reverse
     end
   end
 end
