be4f32ec289dc3202b9a882fab77bb5b8108b934
Shamira <marshallscm@gmail.com>
2019-04-27 14:41:24 -0700

    Added albums, books, and movies to works controller.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,9 @@
 class WorksController < ApplicationController
   def index
     @works = Work.all
+    @albums = Work.where(category: "album")
+    @movies = Work.where(category: "movie")
+    @books = Work.where(category: "book")
   end
 
   def show
@@ -13,5 +16,24 @@ class WorksController < ApplicationController
 
   def new
     @work = Work.new(category: "Category", title: "Title", creator: "Creator", publication_year: "Publication Year", description: "Description")
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+
+    if !@work
+      head :not_found
+    end
+  end
+
+  def destroy
+    work = Work.find_by(id: params[:id])
+
+    if !work
+      head :not_found
+    else
+      work.destroy
+      redirect_to works_path
+    end
   end
 end
