93b53f82d6419a08bd6b427b9cc94b5e4519d4d4
Heather Izumi <hkizumi@gmail.com>
2019-04-27 14:19:27 -0700

    Updated homepage view with top 10

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,4 +6,26 @@ class Work < ApplicationRecord
   def vote_count
     self.votes.length
   end
+
+  def self.top_ten(type)
+    array_by_type = Work.where(category: type)
+
+    array_by_votes = array_by_type.sort_by { |work| -work.votes.count }
+
+    if array_by_votes.length < 10
+      return array_by_votes
+    else
+      return array_by_votes[0..9]
+    end
+  end
+
+  # def top_ten(type)
+  #   array_by_type = self.where(category: type)
+  #   array_by_votes = array_by_type.sort_by { |work| -work.votes.count }
+  #   top_10 = sorted_array[0..9]
+  #   if array_by_votes.length < 10
+  #     return array_by_votes
+  #   else
+  #   return array_by_votes[0..9]
+  # end
 end
