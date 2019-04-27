80acc1c7110692679358b0ee5143ce63c59533f6
K <774162+kaseea@users.noreply.github.com>
2019-04-27 13:59:08 -0700

    no really, now all model testing completed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -14,6 +14,14 @@ describe User do
     expect(user.errors.messages).must_include :username
   end
 
+  it "must require a unique username" do
+    user2 = User.new(username: "this_is_miserable")
+
+    valid_user = user2.valid?
+    expect(valid_user).must_equal false
+    expect(user2.errors.messages).must_include :username
+  end
+
   describe "relationships" do
     it "can have many votes" do
       expect(user.votes.count).must_be :>=, 0
