68a7c2cc7774cf058a0f02fc012b0b95af5ba8b6
Shirley Chu <shirley.cchu@gmail.com>
2019-04-26 22:05:27 -0700

    wrote and passed votes controller tests

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 describe VotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "create (upvote)" do
+    before do
+      @vote = Vote.first
+    end
+
+    it "successfully upvotes" do
+      perform_login(User.first)
+      work = Work.first
+      expect {
+        post work_votes_path(work)
+      }.must_change "Vote.count", +1
+
+      check_flash
+      vote = Vote.last
+      expect(vote.user).must_equal User.first
+      expect(vote.work).must_equal work
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+    end
+
+    it "rejects vote when not logged in" do
+      work = Work.first
+      expect {
+        post work_votes_path(work)
+      }.wont_change "Vote.count"
+
+      check_flash(expected_status = :error)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+
+    end
+
+    it "bans user upvoting same work twice" do
+      user = @vote.user
+      work = @vote.work 
+      perform_login(user)
+      
+      expect {
+        post work_votes_path(work)
+      }.wont_change "Vote.count"
+
+      check_flash(expected_status = :error)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work)
+
+    end
+  end
 end
