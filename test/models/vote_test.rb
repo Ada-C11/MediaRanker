86716aceabd316e62a53b04213d1624acdb2fc91
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-26 19:44:40 -0700

    tests for vote relationships to user and work

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -8,4 +8,16 @@ describe Vote do
 
     expect(valid_vote).must_equal true
   end
+
+  describe "relationships" do
+    it "belongs to a user" do
+      expect(vote.user_id).must_equal vote.user.id
+      expect(vote.user.username).must_equal "obsessed-with-pizza"
+    end
+
+    it "belongs to a work" do
+      expect(vote.work_id).must_equal vote.work.id
+      expect(vote.work.title).must_equal "Grokking Algorithms"
+    end
+  end
 end
