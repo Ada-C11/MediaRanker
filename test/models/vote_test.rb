10fe1fc7e66e811b7a2407c55140f4418d8c58d7
Angela <angela.ohh@gmail.com>
2019-04-27 12:30:12 -0700

    added vote.rb tests for validation

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,18 @@
 require "test_helper"
 
-# describe Vote do
-#   let(:vote) { Vote.new }
+describe Vote do
+  it "must be valid" do
+    vote = Vote.create(user_id: users(:one).id, work_id: works(:one).id)
+    expect(vote.valid?).must_equal true
+  end
 
-#   it "must be valid" do
-#     value(vote).must_be :valid?
-#   end
-# end
+  describe "validations" do
+    it "requires a unique user_id and work_id" do
+      vote = Vote.create(user_id: users(:one).id, work_id: works(:one).id)
+      dup_vote = Vote.new(user_id: users(:one).id, work_id: works(:one).id)
+      expect(dup_vote.save).must_equal false
+      expect(dup_vote.errors.messages).must_include :user_id
+      expect(dup_vote.errors.messages[:user_id]).must_equal ["has already been taken"]
+    end
+  end
+end
