9b2d832fad0c87f3850da5f3ec7af8d9f5cf6c59
Angela <angela.ohh@gmail.com>
2019-04-27 12:31:23 -0700

    erased unnecessary comments in vote.rb

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,4 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
   validates :user_id, uniqueness: { scope: :work_id }
-
-  #   def top_ten_books
-  #     top_books = self.
-  #   end
 end
