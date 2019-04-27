f06fcd8f6e7cb96f847a2bbe4bde910b30656c43
qqdipps <sdipp007@gmail.com>
2019-04-26 20:20:24 -0700

    wrote scafalding for tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,10 +8,28 @@ describe UsersController do
     end
   end
 
-  #   it "should get show" do
-  #     get users_show_url
-  #     value(response).must_be :success?
-  #   end
+  describe "users#show" do
+    it "should get show with valid id" do
+    end
+
+    it "should return a 404 with invalid id" do
+    end
+  end
+
+
+  describe "users#login" do 
+    it "will create a new user if username does not exist" do 
+    end
+
+    it "will update session user to previously created user" do
+    end
+  end
+
+  describe "user#logout" do 
+    it "will log out user" do 
+    end
+  end
+  
 
   #   it "should get new" do
   #     get users_new_url
