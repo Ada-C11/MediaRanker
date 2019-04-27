0fe51573287e09f8ff3271c2151e4a558d14ec3d
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-27 15:27:10 -0700

    removed unneccesary method from app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def display_username(session_user_id)
-    return "[unknown]" unless session_user_id
-    user ||= User.find(session_user_id) if session_user_id
-    return (user.username)
-  end
+
 end
