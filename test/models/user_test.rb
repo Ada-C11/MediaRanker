6a54d1c2c38c898124a258be406392c20e9f66e9
K <774162+kaseea@users.noreply.github.com>
2019-04-27 13:51:58 -0700

    all model testing completed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { users(:one) }
 
   it "must be valid" do
     value(user).must_be :valid?
+  end
+
+  it "must require a username" do
+    user.username = ""
+
+    expect(user.valid?).must_equal false
+    expect(user.errors.messages).must_include :username
+  end
+
+  describe "relationships" do
+    it "can have many votes" do
+      expect(user.votes.count).must_be :>=, 0
+      user.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
+    end
   end
 end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:vote) { votes(:vote_1) }
 
   it "must be valid" do
     value(vote).must_be :valid?
+  end
+
+  it "must require a user_id" do
+    vote.username = ""
+
+    expect(user.valid?).must_equal false
+    expect(user.errors.messages).must_include :username
+  end
+
+  describe "relationships" do
+    it "can have many votes" do
+      expect(user.votes.count).must_be :>=, 0
+      user.votes.each do |vote|
+        expect(vote).must_be_instance_of Vote
+      end
+    end
   end
 end
