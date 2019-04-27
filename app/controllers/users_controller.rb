7b0efeb34866255349176360ad0057500ee6aee4
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-04-27 14:52:44 -0700

    fixed routing issue for user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,14 +33,12 @@ class UsersController < ApplicationController
   end
 
   def current
-    current_user = User.find_by(id: session[:user_id])
+    @current_user = User.find_by(id: session[:user_id])
 
-    unless current_user
+    unless @current_user
       flash[:error] = "You must be logged in to see this page!"
       redirect_to root_path
     end
-
-    redirect_to user_path(current_user.id)
   end
 
   def logout
