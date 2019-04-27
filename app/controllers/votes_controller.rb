3ea3270f96b0d5c9675bf04001054d27be519200
Heather Izumi <hkizumi@gmail.com>
2019-04-26 20:40:38 -0700

    Votes Controller updated upvote method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,18 +2,18 @@ class VotesController < ApplicationController
   before_action :find_user, only: [:upvote]
 
   def upvote
-    # @user = User.find_by(id: session[:user_id])
+    # find_user method in application controller
     @work = Work.find_by(id: params[:id])
 
     @vote = Vote.new(user_id: @user.id, work_id: @work.id)
 
     if @user.nil?
       flash[:error] = "You must log in to do that"
-      #   redirect_to works_path
+      redirect_to works_path
     else
       @vote.save
       flash[:success] = "Successfully upvoted!"
-      #   redirect_to works_path
+      redirect_to works_path
     end
   end
 end
