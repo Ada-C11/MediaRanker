60dcc752882f041a112312bfd266d3cb9ccd1e55
MyriamWD <myriam.waldend@gmail.com>
2019-04-28 09:43:01 -0700

    top ten works relocating business logic

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,10 @@
 class HomePageController < ApplicationController
   def index
+    @books = Work.books.order("lower(publication_year
+      ) DESC")
+    # @albums = Work.albums.order("lower(publication_year
+    #     ) DESC")
+    # @movies = Work.movies.order("lower(publication_year
+    #       ) DESC")
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -14,4 +14,19 @@ class Work < ApplicationRecord
     spotlight_criteria = self.maximum("publication_year")
     spotlight = self.find_by(publication_year: spotlight_criteria)
   end
+
+  def self.top(category)
+    full_collection = Work.where(category: category).order("lower(publication_year) DESC")
+    top_ten = []
+    position = 0
+    full_collection.each do |work|
+      if position <= 9
+        top_ten << work
+      else
+        return top_ten
+      end
+      position += 1
+    end
+    #return top_ten
+  end
 end
