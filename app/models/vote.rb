3dfe4cc65f88b4cb9e0db81d17ffa025f419b93f
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-28 10:13:31 -0700

    removed custom methods top_ten and spotlight from votes model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,24 +1,4 @@
 class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
-
-  def self.top_ten(category)
-    works = Work.where(category: category)
-
-    top_works = works.sort_by { |work| work.votes.length }
-
-    if top_works.length > 10
-      return top_works.reverse.first(10)
-    else
-      return top_works.reverse
-    end
-  end
-
-  def self.spotlight
-    works = Work.all
-
-    spotlight = works.sort_by { |work| work.votes.length }
-
-    return spotlight.last
-  end
 end
