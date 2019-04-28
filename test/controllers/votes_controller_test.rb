52aba2af82d9d48a61aa32ebd1632a5454c9f8de
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-27 21:56:23 -0700

    added testing for votes controller create action

--- test/controllers/votes_controller_test.rb
+++ test/controllers/votes_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe VotesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:work) { works(:paint_it_black) }
+
+  describe "create" do
+    it "can create a vote for a user for a given work" do
+      logged_in_user = perform_login
+
+      expect {
+        post work_votes_path(work.id)
+      }.must_change "Vote.count", 1
+
+      expect(flash[:success]).must_equal "Successfully upvoted!"
+      must_respond_with :redirect
+      # only tests the fallback location, is there a test for redirect back? Internet said 'nah'.
+      must_redirect_to root_path
+    end
+
+    it "will not allow a user to vote unless they are logged in" do
+      expect {
+        post work_votes_path(work.id)
+      }.wont_change "Vote.count"
+
+      expect(flash[:failure]).must_equal "You must log in to do that"
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it "will not allow a user to vote for the same work more than once" do
+      logged_in_user = perform_login
+      logged_in_user.works << work
+
+      expect {
+        post work_votes_path(work.id)
+      }.wont_change "Vote.count"
+
+      expect(flash[:failure]).must_equal "Could not upvote"
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+  end
 end
