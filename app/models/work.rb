f078f1e54f222786b5c383d0ef533b0afb5032a0
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-28 10:13:48 -0700

    added custom methods top_ten and spotlight to works model

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,4 +5,24 @@ class Work < ApplicationRecord
   validates :category, presence: true
   validates :title, presence: true, uniqueness: true
   validates :creator, presence: true
+
+  def self.top_ten(category)
+    works = self.where(category: category)
+
+    top_works = works.sort_by { |work| work.votes.length }
+
+    if top_works.length > 10
+      return top_works.reverse.first(10)
+    else
+      return top_works.reverse
+    end
+  end
+
+  def self.spotlight
+    works = self.all
+
+    spotlight = works.sort_by { |work| work.votes.length }
+
+    return spotlight.last
+  end
 end
