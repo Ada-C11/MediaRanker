35119e0352211c434abb6c87fe3ed66c6b119ccf
Shubha Rajan <shubharajan94@gmail.com>
2019-04-28 00:06:49 -0700

    recommendation methods are working! (tested in console with pry)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,33 @@ class User < ApplicationRecord
     other_works = other.votes.map { |vote| vote.work }
     common_works = user_works & other_works
     upvoted_works = (user_works + other_works).uniq
-    return (common_works.count) / (upvoted_works.count)
+    return (common_works.count.to_f) / (upvoted_works.count.to_f)
   end
 
   # get_similar_users returns a list of all other users stored in db, ranked by similarity.
   # Useful for implementing a friend recommendation system
   def get_similar_users
-    users = User.all.clone
-    other_users = users.delete(user)
-    sorted_by_similarity = other_users.sort_by { |other| j_index(self, other) }.reverse
-    return sorted_by_similarity
-    return
+    users = User.pluck(:id)
+    users.delete(self.id)
+    sorted_by_similarity = users.sort_by { |other_id| j_index(User.find(other_id)) }.reverse
+    return sorted_by_similarity.map { |user_id| User.find(user_id) }
   end
 
   # get_recommendations returns a list of works by other users ranked by the average
   # j_index of other users who liked the work, and the user the method is called on.
-  def get_reccommendations
-    reccommendations = Hash.new { |hash, key| hash[key] = 0 }
-    users = User.all.clone
-    other_users = users.delete(user)
-    other_users.each do |other|
-      difference = self.votes.map { |vote| vote.work.id } - other.votes.map { |vote| vote.work.id }
+  def get_recommendations
+    recommendations = Hash.new { |hash, key| hash[key] = { j_index_sum: 0, users_count: 0 } }
+    users = User.pluck(:id)
+    users.delete(self.id)
+    users.each do |other_id|
+      other = User.find(other_id)
+      difference = other.votes.map { |vote| vote.work.id } - self.votes.map { |vote| vote.work.id }
       difference.each do |work_id|
-        reccommendations["#{work_id}"][j_index_sum] += j_index(other)
-        reccommendations["#{work_id}"][users_count] += 1
+        recommendations["#{work_id}"][:j_index_sum] += j_index(other)
+        recommendations["#{work_id}"][:users_count] += 1
       end
-      ranked_recommendations = reccommendations.keys.sort_by { |key| reccommendations[key][j_index_sum] / reccommendations[key][user_count] }
-      return ranked_recommendations.reverse
+      ranked_recommendations = recommendations.keys.sort_by { |key| recommendations[key][:j_index_sum] / recommendations[key][:users_count] }
+      return ranked_recommendations
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "faker"
 
 MEDIA_FILE = Rails.root.join("db", "media_seeds.csv")
 
@@ -22,3 +23,38 @@ end
 
 puts "Added #{Work.count} work records"
 puts "#{work_failures.length} works failed to save"
+
+user_failures = []
+15.times do
+  user = User.new(username: Faker::Movies::PrincessBride.unique.character)
+  successful = user.save
+  if !successful
+    user_failures << user
+    puts "Failed to save user: #{user.inspect}"
+  else
+    puts "Created user: #{user.inspect}"
+  end
+end
+
+puts "Added #{User.count} user records"
+puts "#{user_failures.length} users failed to save"
+
+vote_failures = []
+User.all.each do |user|
+  Work.all.each do |work|
+    randomizer = rand(2)
+    if randomizer == 1
+      vote = Vote.new(user_id: user.id, work_id: work.id)
+      successful = vote.save
+      if !successful
+        vote_failures << vote
+        puts "Failed to save vote: #{vote.inspect}"
+      else
+        puts "Created vote: #{vote.inspect}"
+      end
+    end
+  end
+end
+
+puts "Added #{Vote.count} vote records"
+puts "#{vote_failures.length} votes failed to save"
