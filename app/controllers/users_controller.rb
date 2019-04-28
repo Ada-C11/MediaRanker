3b9302a4182d399702582dedf66ed9c1dcefa392
Erica Norris <norrise120@gmail.com>
2019-04-28 16:28:52 -0700

    wrote tests and user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,40 @@
 class UsersController < ApplicationController
+  def login_form
+    @user = User.new
+  end
+
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as existing user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+
+    redirect_to root_path
+  end
+
+  def logout
+    session[:user_id] = nil
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to root_path
+  end
+
+  def current
+    @user = User.find_by(id: session[:user_id])
+
+    unless @user
+      flash[:status] = :error
+      flash[:message] = "You must be logged in to see this page"
+      redirect_to login_path
+      return
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
-describe UsersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+describe "UsersController" do
+  describe "login_form" do
+    it "can render the login form" do
+      get login_path
+      must_respond_with :ok
+    end
+  end
+
+  describe "current" do
+    it "returns 200 OK for a logged-in user" do
+      user = User.first
+      login_data = {
+        user: {
+          username: user.username,
+        },
+      }
+      post login_path, params: login_data
+
+      expect(session[:user_id]).must_equal user.id
+
+      get current_user_path
+
+      must_respond_with :success
+    end
+  end
 end
