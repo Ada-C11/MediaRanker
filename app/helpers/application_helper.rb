7acb1c7d2184d663eb240c58e8579564c40cd351
Carla Bosco <carlabosco@gmail.com>
2019-04-28 15:56:25 -0700

    Debugging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,12 @@
 module ApplicationHelper
-  def flash_class(level)
-    case level
-      when :notice then "alert alert-info"
-      when :success then "alert alert-success"
-      when :error then "alert alert-error"
-      when :alert then "alert alert-error"
-    end
-  end
+  
 
   def show_spotlight
     works = Work.all.to_a
-    works.sort_by! { |work| Vote.where(work_id: work.id).length }
-    if works[0].nil?
-      flash[:notice] = "Start voting to see media on the Spotlight!"
+    if works.nil?
+      flash[:message] = "Start voting to see media on the Spotlight!"
     else
+      works.sort_by! { |work| Vote.where(work_id: work.id).length }
       return works.reverse[0]
     end
   end
