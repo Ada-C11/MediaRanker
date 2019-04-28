26a5dafd564777f0eaa8c4c1440125f4347c574e
Bitaman <b.amani.s@gmail.com>
2019-04-28 06:11:13 -0700

    added fixtures for work, vote and user

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,13 +2,12 @@ require "test_helper"
 
 describe Work do
   # let(:work) { Work.new }
+  before do
+    @work = Work.new(
+      title: "some unique test title",
+    )
+  end
   describe "validations" do
-    before do
-      @work = Work.new(
-        title: "some unique test title",
-      )
-    end
-
     it "passes validations with good data" do
       expect(@work).must_be :valid?
     end
@@ -26,5 +25,24 @@ describe Work do
       expect(@work.errors.messages).must_include :title
     end
   end
-  
+  describe "relations" do
+    it "has votes" do
+      work = Work.create!(title: "test")
+      user = User.create!(username: "test")
+      Vote.create!(work_id: work.id, user_id: user.id)
+      # Vote.create(work_id: @work.id, user_id: 4)
+
+      expect(@work.votes.length).must_equal 1
+      expect(vote.work_id).must_equal @work.id
+    end
+  end
+
+  describe "spotligth" do
+  end
+  describe "top ten movies" do
+  end
+  describe "top ten books" do
+  end
+  describe "top ten album" do
+  end
 end
