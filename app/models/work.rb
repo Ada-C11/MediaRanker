aa1c3292c1e3ee895253ad7eae92d3e3f5b8d3d7
Nara <naraa_520@yahoo.com>
2019-04-27 19:11:19 -0700

    Creates vote model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,7 @@
 class Work < ApplicationRecord
   belongs_to :category
   validates :title, presence: true, uniqueness: true
+
+  has_many :votes
+  has_many :users, through: :votes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_230609) do
+ActiveRecord::Schema.define(version: 2019_04_28_013445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,21 @@ ActiveRecord::Schema.define(version: 2019_04_22_230609) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
