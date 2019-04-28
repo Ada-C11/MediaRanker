e9c0cbd39971523ea08594e39bcdc54ca05750e4
Jessica <jessica.homet@gmail.com>
2019-04-28 08:40:10 -0700

    Re-add user model tests (incorrect)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :works, through: :votes
 
+  # I added this before I decided to do a current user path. Do I need this, then?
   def self.current(session)
     return nil unless session[:user_id]
 
