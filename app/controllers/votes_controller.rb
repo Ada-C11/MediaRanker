a26a913aa8a51d5b6061314109a7155f40d3fe20
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-27 21:38:08 -0700

    added user can only vote once per media functionality to create action in votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,16 @@ class VotesController < ApplicationController
   def create
     @work = Work.find_by(id: params[:work_id])
     @user = User.find_by(id: session[:user_id])
-    @work.users << @user
+    @vote = @user.works.find_by(id: params[:work_id])
+
+    if !@user
+      flash[:failure] = "You must log in to do that"
+    elsif @vote
+      flash[:failure] = "Could not upvote"
+    else
+      @work.users << @user
+      flash[:success] = "Successfully upvoted!"
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
