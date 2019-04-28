91cbaed6d4facfa78c8e9f9225a3aab95678ddb5
Heather Izumi <hkizumi@gmail.com>
2019-04-28 15:57:50 -0700

    Test for Homepages Controller

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 describe HomepagesController do
-  it "should get index" do
+  describe "index" do
+    it "should get index" do
+      get works_path
+
+      must_respond_with :success
+    end
   end
 end
