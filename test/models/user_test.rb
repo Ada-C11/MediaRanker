bc45389ab97809866aff417b06fa387fee96a430
Jessica <jessica.homet@gmail.com>
2019-04-28 08:39:38 -0700

    Re-add user model tests (incorrect)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,19 @@ describe User do
   it "must be valid" do
     value(user).must_be :valid?
   end
+
+  # Does this belong here???
+  it "returns 200 OK for a logged-in user" do
+    user = User.first
+    login_data = {
+      user: {
+        username: user.name,
+      },
+    }
+    post login_path, params: login_data
+  end
+
+  expect(session[:user_id]).must_equal user.id
+
+  # get current_user_pa
 end
