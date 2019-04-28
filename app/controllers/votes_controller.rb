21c2c23fc19725a91eb56634c40e61a4ee487da8
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-27 21:11:41 -0700

    deleted unnecessary comment

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,5 @@
 class VotesController < ApplicationController
   def create
-    # raise
     @work = Work.find_by(id: params[:work_id])
     @user = User.find_by(id: session[:user_id])
     @work.users << @user
