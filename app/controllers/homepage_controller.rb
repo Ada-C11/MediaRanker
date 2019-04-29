f646eff3b779b48a7e041ca851ca4f7c77d83100
Ngoc Le <ngocle@outlook.com>
2019-04-28 20:42:52 -0700

    added find_most_voted and find_top_ten methods to work model

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,4 @@
 class HomepageController < ApplicationController
   def index
-    movies = Work.where(category: "movie")
-    @top_10_movies = movies.sample(10)
-
-    albums = Work.where(category: "album")
-    @top_10_albums = albums.sample(10)
-
-    books = Work.where(category: "book")
-    @top_10_books = books.sample(10)
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,22 @@
 class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   has_many :votes
+
+  def self.find_top_ten(category_name)
+    works = Work.where(category: category_name).sort_by { |work| work.vote_ids.length }.reverse!
+    return works[0, 9]
+  end
+
+  def self.find_most_voted
+    max_votes = 0
+    most_voted = nil
+    Work.all.each do |work|
+      work_votes = work.vote_ids.length
+      if work_votes >= max_votes
+        max_votes = work_votes
+        most_voted = work
+      end
+    end
+    return most_voted
+  end
 end
