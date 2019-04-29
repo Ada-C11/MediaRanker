76ab61399a47abdfd1129150db0bd302e81327fb
Heather Izumi <hkizumi@gmail.com>
2019-04-28 23:11:04 -0700

    added edge case tests to work model top_ten test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -43,11 +43,23 @@ describe Work do
   end
 
   describe "self.top_ten method" do
-    it "must return the top ten votes, based on works" do
+    it "must return the top ten works, based on votes" do
       top_array = Work.top_ten("album")
 
       expect(top_array.length).must_equal 10
       expect(top_array[0][:title]).must_equal "Test Album4"
+    end
+
+    it "will return few works if there are not at least 10 in a category" do
+      top_array = Work.top_ten("book")
+
+      expect(top_array.length).must_equal 2
+    end
+
+    it "will return an empty array if there are no works in the category" do
+      top_array = Work.top_ten("movie")
+
+      expect(top_array).must_equal []
     end
   end
 
