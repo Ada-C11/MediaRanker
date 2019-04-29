980fed1e937c95878902c8eece49903a1c702867
Heather Izumi <hkizumi@gmail.com>
2019-04-28 21:23:06 -0700

    Vote model validation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,44 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:user) { users(:one) }
+  let(:work) { works(:album) }
+  let(:vote) { votes(:vote1) }
 
   it "must be valid" do
-    # value(vote).must_be :valid?
+    expect(vote.valid?).must_equal true
+  end
+
+  describe "validations" do
+    it "requires a user_id and work_id" do
+      vote.user = nil
+      expect(vote.valid?).must_equal false
+
+      vote.user = user
+      expect(vote.valid?).must_equal true
+
+      vote.work = nil
+      expect(vote.valid?).must_equal false
+
+      vote.work = work
+      expect(vote.valid?).must_equal true
+    end
+
+    it "allows only 1 vote per user for a specific work" do
+      first_vote = Vote.new
+      first_vote.user = user
+      first_vote.work = work
+
+      expect(first_vote.save).must_equal true
+
+      second_vote = Vote.new
+      second_vote.user = user
+      second_vote.work = work
+
+      expect(second_vote.save).must_equal false
+    end
+  end
+
+  describe "relationships" do
   end
 end
