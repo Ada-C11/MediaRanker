92a39d3ef1ce5f6752622ece05966fe388299b1a
Ngoc Le <ngocle@outlook.com>
2019-04-29 13:01:27 -0700

    debugged

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "homepage#index"
+  root :to => "homepage#index"
   resources :works do
     resources :votes, only: [:create]
   end
