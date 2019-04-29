b263398f0c83eaebd51a862e1535cab3cb340da0
Jessica <jessica.homet@gmail.com>
2019-04-28 22:46:05 -0700

    Bug fix attempt: commenting out migration

--- db/migrate/20190423034153_remove_join_date_from_users_take_2.rb
+++ db/migrate/20190423034153_remove_join_date_from_users_take_2.rb
@@ -1,5 +1,5 @@
-class RemoveJoinDateFromUsersTake2 < ActiveRecord::Migration[5.2]
-  def change
-    remove_column(:users, :join_date)
-  end
-end
+# class RemoveJoinDateFromUsersTake2 < ActiveRecord::Migration[5.2]
+#   def change
+#     remove_column(:users, :join_date)
+#   end
+# end
