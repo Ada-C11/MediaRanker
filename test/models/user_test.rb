063e8c087347cdd4bdb095368d5c58a886d95049
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-30 07:32:05 -0700

    testing for user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,31 @@ describe User do
   it "must be valid" do
     value(user).must_be :valid?
   end
+
+  describe "validations" do
+    it "must have a username" do
+      user.username = nil
+
+      valid_user = user.valid?
+
+      expect(valid_user).must_equal false
+
+      expect(user.errors.messages).must_include :username
+      expect(user.errors.messages[:username]).must_equal ["can't be blank"]
+    end
+
+    it "must have a join date" do
+      user.join_date = nil
+
+      valid_user = user.valid?
+
+      expect(valid_user).must_equal false
+
+      expect(user.errors.messages).must_include :join_date
+      expect(user.errors.messages[:join_date]).must_equal ["can't be blank"]
+    end
+  end
+
+  describe "relations" do
+  end
 end
