f41b9851afb5ea9770e404b7df1a85a0392dfcba
Chantal Demissie <chantaldemissie@yahoo.com>
2019-05-05 22:54:45 -0700

    upvote and downvotes methods complete and in views

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,8 +2,18 @@ class Work < ApplicationRecord
   has_many :votes
 
   def votes
-    votes = Vote.where("work_id = ?", self.id)
-    puts "vote.all.count", Vote.all.count
-    votes
+    Vote.where(work_id: self.id)
+  end
+
+  def upvotes
+    Vote.where(work_id: self.id, value: 1)
+  end
+
+  def downvotes
+    Vote.where(work_id: self.id, value: -1)
+  end
+
+  def top_voted
+    Work.all.sort { |work| work.votes.sum { |vote| vote.value } }.first
   end
 end
