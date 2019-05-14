5bfdb36101a8a2b069a3ed0010778172b8957c51
Maria Wissler <wmcarmelina@gmail.com>
2019-05-13 23:34:39 -0700

    added actions to works controllers

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-    @works = Works.all
+    @works = Work.all
   end
 
   def show
@@ -18,20 +18,49 @@ class WorksController < ApplicationController
   def create
     @work = Works.new(work_params)
 
-    succesful = @work.save
-     redirect_to work_path(@work.id)
+    is_successful = @work.save
+
+    if is_successful
+      flash[:success] = "#{@work.title} was successfully created."
+      redirect_to work_path(@work.id)
     else
-     render :new, status :bad_request
-    end 
-   end  
+      flash.now[:failure] = "Unable to create #{@work.category}"
+      @work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+      render :new, status: :bad_request
+    end
+  end
 
   def edit
+    @work = Work.find_by(id: params[:id])
+
+    unless @work
+      head :not_found
+    end
   end
 
   def update
+    if @work.update(work_params)
+      flash[:success] = "Successfully updated #{@work.title}"
+      redirect_to work_path(@work.id)
+    else
+      flash.now[:error] = "Unable to update"
+      render(:edit, status: :bad_request)
+    end
   end
 
   def destroy
+    work = Work.find_by(id: params[:id])
+
+    if !work
+      flash[:failure] = "Unable to delete"
+      redirect_to root_path
+    else
+      work.destroy
+      flash[:success] = "Success! #{work.title} is now deleted."
+      redirect_to works_path
+    end
   end
 
   private
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,4 +6,25 @@ describe Work do
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  describe "validations" do
+    it "must have a title" do
+      work.title = nil
+
+      valid_work = work.valid?
+
+      expect(valid_work).must_equal false
+      expect(work.errors.messages).must_include :title
+      expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
+
+    it "must have a unique title" do
+      identical_title = Work.new(title: "Lemonade", creator: "Beyonce")
+
+      expect(identical_title.save).must_equal false
+
+      expect(identical_title.errors.messages).must_include :title
+      expect(identical_title.errors.messages[:title]).must_equal ["has already been taken"]
+    end
+  end
 end
