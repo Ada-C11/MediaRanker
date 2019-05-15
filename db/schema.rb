db51dd7d45a4ad9247f35ccd0459d78976c2601f
Maria Wissler <wmcarmelina@gmail.com>
2019-05-14 22:58:27 -0700

    generated controllers, checked route view for homepage

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_05_15_051356) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "{:foreign_key=>true}_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_users_on_{:foreign_key=>true}_id"
   end
 
   create_table "votes", force: :cascade do |t|
@@ -38,6 +40,8 @@ ActiveRecord::Schema.define(version: 2019_05_15_051356) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "{:foreign_key=>true}_id"
+    t.index ["{:foreign_key=>true}_id"], name: "index_works_on_{:foreign_key=>true}_id"
   end
 
   add_foreign_key "votes", "users"
