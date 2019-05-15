dde640e0db92047af976811f0f9be60052786f49
Maria Wissler <wmcarmelina@gmail.com>
2019-05-14 23:07:06 -0700

    added validations and index view for work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :votes
+  has_many :works, through: :votes
+
+  validates :username, presence: true
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,10 @@
 class Work < ApplicationRecord
   has_many :votes
+  has_many :users, through: :votes
+
+  validates :title, presence: true
+  validates :publication_year, presence: true
+  validates :creator, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
 end
