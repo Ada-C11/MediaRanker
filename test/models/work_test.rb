f65e638ba8f9501944d7bca3bd819c5fe3347827
Maria Wissler <wmcarmelina@gmail.com>
2019-05-20 15:07:55 -0700

    start model test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,28 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:one) }
 
   it "must be valid" do
-    value(work).must_be :valid?
+    work = works(:one)
+
+    valid_work = work.valid?
+
+    expect(valid_work).must_equal true
+  end
+
+  describe "validations" do
+    it "requires a title" do
+      # Arrange
+      work.title = nil
+
+      # Act
+      valid_work = work.valid?
+
+      # Assert
+      expect(valid_work).must_equal false
+      expect(work.errors.messages).must_include :title
+      expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
   end
 end
