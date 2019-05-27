0ae744b8c0391cf27f2e8582564e36d544d920bf
laneia <laneiamurray@gmail.com>
2019-05-26 22:03:31 -0400

    finished seeds.rb setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
 require "csv"
 
 SEEDS = Rails.root.join("db", "media_seeds.csv")
+
+seed_fails = []
+
+CSV.foreach(SEEDS, :headers => true) do |seed|
+  work = Work.new
+  work.category = seed["category"]
+  work.title = seed["title"]
+  work.creator = seed["creator"]
+  work.publication_year = seed["publication_year"]
+  work.description = seed["description"]
+
+  success = work.save
+  if !success
+    seed_fails << work
+    puts seed_fails
+  end
+end
+
+puts "#{seed_fails.length} failed to save."
