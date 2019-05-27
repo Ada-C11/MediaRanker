7b87a8232304e60cac9257d3335ea8545219cd74
laneia <laneiamurray@gmail.com>
2019-05-26 22:24:22 -0400

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_013655) do
+ActiveRecord::Schema.define(version: 2019_05_27_022048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2019_05_27_013655) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -38,4 +42,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_013655) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
