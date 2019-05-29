1c0d52684ee10efea26f86b9dcd557be5ec3cb16
laneia <laneiamurray@gmail.com>
2019-05-29 16:21:23 -0700

    added index and show to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @users = User.find_by(id: params[:id].to_i)
+
+    if @user.nil?
+      render :notfound, status: :not_found
+    end
+  end
 end
