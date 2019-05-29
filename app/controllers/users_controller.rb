608061b1f48809a8e8c53fd593174a84c2d4d8e2
laneia <laneiamurray@gmail.com>
2019-05-29 16:22:49 -0700

    added new and create to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
       render :notfound, status: :not_found
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.joined = Date.today
+
+    if @user.save
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
 end
