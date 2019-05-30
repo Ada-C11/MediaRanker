e7e93a957340e26051c703204e1edcabca67c7fa
laneia <laneiamurray@gmail.com>
2019-05-30 11:59:23 -0700

    added work model tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,28 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:two) }
 
   it "must be valid" do
     value(work).must_be :valid?
+  end
+
+  describe "relationships" do
+    it "has many votes" do
+      expect(work).must_be_instance_of Work
+      expect(work.votes).must_be_instance_of Array
+      expect(work.votes.first).must_be_instance_of Vote
+    end
+  end
+
+  describe "validations" do
+    it "must have a title" do
+      work.title = nil
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :title
+      expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+    end
   end
 end
