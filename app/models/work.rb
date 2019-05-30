1fc683e6468a5d9adf5d58f6bfbefa84077b107b
laneia <laneiamurray@gmail.com>
2019-05-30 08:21:32 -0700

    added self methods to sort through types of media

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,16 @@ class Work < ApplicationRecord
   has_many :votes
 
   validates :title, presence: true
+
+  def self.albums
+    Work.select { |work| work.category == "album" }
+  end
+
+  def self.books
+    Work.select { |work| work.category == "book" }
+  end
+
+  def self.movies
+    Work.select { |work| work.category == "movie" }
+  end
 end
