e9b2fc025650d6039e61b1c642608868458fa6a8
laneia <laneiamurray@gmail.com>
2019-05-30 05:50:56 -0700

    added index and show to works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,13 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by(id: params[:id].to_i)
+
+    if @work.nil?
+      render :notfound, status: :not_found
+    end
+  end
 end
