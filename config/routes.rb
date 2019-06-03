0ab3e7e37165b492a3b24d267ea1a1a04e7db552
laneia <laneiamurray@gmail.com>
2019-06-03 14:47:48 -0700

    added sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  get "sessions/login", to: "sessions#new"
+  post "sessions/login", to: "sessions#login", as: "login"
+  delete "sessions/destroy", to: "sessions#destroy", as: "logout"
+
   resources :works
   resources :users, only: [:index, :show, :new, :create]
 end
