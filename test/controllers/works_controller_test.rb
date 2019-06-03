beac71ef9909ae7ef3f99ed0b72c545008e0f0be
laneia <laneiamurray@gmail.com>
2019-06-03 15:01:39 -0700

    added works controller tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 describe WorksController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "can list all works" do
+    get works_index_url
+    value(response).must_be :success?
+  end
+
+  it "can show a work" do
+    get works_show_url
+    value(response).must_be :success?
+  end
+
+  it "can get new work url" do
+    get works_new_url
+    value(response).must_be :success?
+  end
+
+  it "can create a new work" do
+    get works_create_url
+    value(response).must_be :success?
+  end
+
+  it "can edit a work" do
+    get works_edit_url
+    value(response).must_be :success?
+  end
 end
