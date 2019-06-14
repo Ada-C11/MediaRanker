59b6afadfc41bc8ded8d6007c24efdbd32e1204a
laneia <laneiamurray@gmail.com>
2019-06-14 07:42:23 -0700

    added upvote method to works controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,14 +3,6 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
-  def show
-    @work = Work.find_by(id: params[:id].to_i)
-
-    if @work.nil?
-      render :notfound, status: :not_found
-    end
-  end
-
   def new
     @work = Work.new
   end
@@ -23,14 +15,8 @@ class WorksController < ApplicationController
       redirect_to work_path(@work.id)
     else
       flash.now[:error] = "Error:  work not added"
-      @work.errors.messages.each do |field, messages|
-        flash.now[field] = messages
-      end
     end
-  end
-
-  def edit
-    @work - Work.find_by(id: params[:id].to_i)
+    render :new
   end
 
   def update
@@ -49,11 +35,28 @@ class WorksController < ApplicationController
 
   def destroy
     unless @work.nil?
-      work = Work.find_by(id: params[:id].to_i)
       deleted_work = @work.destroy
       flash[:success] = "#{deleted_work.title} deleted"
-
       redirect_to root_path
+    end
+  end
+
+  def upvote
+    user = User.find_by(id: session[:user_id])
+
+    if user.nil?
+      error_redirect(@work, "A problem occurred: you must log in to vote")
+    elsif user.eligible_to_vote?(@work)
+      vote = Vote.new(work: @work, user: user, date: Date.today)
+
+      if vote.save
+        flash[:success] = "Successfully upvoted!"
+        redirect_back(fallback_location: root_path)
+      else
+        error_redirect(@work, "Error: could not process vote")
+      end
+    else
+      error_redirect(@work, "A problem occurred: you've already voted on this work")
     end
   end
 
