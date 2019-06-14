ad9181ea2bc27dc486ca2412bc2320dc3f53e083
laneia <laneiamurray@gmail.com>
2019-06-14 07:53:32 -0700

    added vote methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  has_many :votes
+  has_many :votes, dependent: :delete_all
   has_many :works, through: :votes
 
-  validates :username, presence: true
+  validates :username, :joined, presence: true
+
+  def vote_date(work)
+    vote = self.votes.find_by(work_id: work.id)
+    return vote ? vote.date : nil
+  end
+
+  def vote_count
+    return self.votes.length
+  end
+
+  def eligible_to_vote?(work)
+    return !(works.include? work)
+  end
 end
