0cdf0b5241ea8f00d9924307b3ad021bbab42e51
laneia <laneiamurray@gmail.com>
2019-06-14 06:37:10 -0700

    added error handling to login sessions controller and minor changes to flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,32 @@
 class SessionsController < ApplicationController
   def login
     if user.nil?
-      user = User.create(name: params[:author][:username])
+      user = User.create(name: params[:author][:username], joined: Date.today)
     end
 
     user = User.find_by(name: params[:user][:username])
 
     session[:user_id] = user.id
 
-    flash[:success] = "Logged in #{user.username}!"
-    redirect_back(fallback_location: root_path)
+    if user.valid?
+      flash[:success] = "#{user.username} successfully logged in"
+      redirect_to root_path
+    else
+      flash[:warning] = "A problem occurred: Could not log in"
+      user.errors.messages.each do |field, messages|
+        flash[field] = messages
+      end
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def new
+    @user = User.new
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "Logged out!"
+    flash[:success] = "Successfully logged out"
     redirect_back(fallback_location: root_path)
   end
 end
