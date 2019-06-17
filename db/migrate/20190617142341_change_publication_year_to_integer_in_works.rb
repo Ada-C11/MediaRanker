ba704ff9aabe89cece98d610636ba98d14bc2774
laneia <laneiamurray@gmail.com>
2019-06-17 09:40:07 -0700

    trying another way to force to int

--- db/migrate/20190617142341_change_publication_year_to_integer_in_works.rb
+++ db/migrate/20190617142341_change_publication_year_to_integer_in_works.rb
@@ -1,5 +1,5 @@
 class ChangePublicationYearToIntegerInWorks < ActiveRecord::Migration[5.2]
   def change
-    change_column :works, :publication_year, :integer, using: "publication_year AS integer"
+    change_column :works, :publication_year, "integer USING CAST(publication_year AS integer)"
   end
 end
