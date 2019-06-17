4c09fa2af18397a5da404997061428be3bfa88dd
laneia <laneiamurray@gmail.com>
2019-06-17 10:10:14 -0700

    trying yet another syntax for migration to int

--- db/migrate/20190617142341_change_publication_year_to_integer_in_works.rb
+++ db/migrate/20190617142341_change_publication_year_to_integer_in_works.rb
@@ -1,5 +1,5 @@
 class ChangePublicationYearToIntegerInWorks < ActiveRecord::Migration[5.2]
   def change
-    change_column :works, :publication_year, "integer USING CAST(publication_year AS integer)"
+    execute "ALTER TABLE works ALTER COLUMN publication_year TYPE integer USING (publication_year::integer"
   end
 end
