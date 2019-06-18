9c0fa3afb2d4ea7da4cdf10660729f4156fd7e85
laneia <laneiamurray@gmail.com>
2019-06-18 11:20:00 -0700

    users controller tests passing

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,13 @@
 require "test_helper"
 
 describe SessionsController do
-  it "can login" do
+  it "can login a valid user" do
+    user = users(:one)
     get sessions_login_path
-    value(response).must_be :successful?
+
+    #assert_equal "#{user.username} successfully logged in", flash[:success]
+    flash[:success].should =~ /#{user.username} successfully logged in/i
+    must_redirect_to root_path
   end
 
   it "can logout" do
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 describe UsersController do
-  it "can get a list of all users" do
-    get users_index_url
+  it "returns a list of all users" do
+    get users_path
     must_respond_with :success
   end
 
-  it "can show a user" do
-    get users_show_url
+  it "shows a valid user" do
+    user = users(:one)
+    get user_path(user.id)
     must_respond_with :success
+  end
+
+  it "returns error for invalid user" do
+    get user_path(-3)
+    assert_equal "User not found.", flash[:error]
   end
 end
